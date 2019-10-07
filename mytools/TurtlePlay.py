7d4704017f269f1e2848fce88f303b34e21cbab0
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-07 09:27:02 +0800

    add some code annotations

--- mytools/TurtlePlay.py
+++ mytools/TurtlePlay.py
@@ -24,36 +24,51 @@ class TurtlePlay():
     """docstring for TurtlePlay"""
     def __init__(self):
         super(TurtlePlay, self).__init__() 
+        # theta: rotation angle for each time
         self.theta = None 
+        # frames per second
+        # such as : self.speed = 4
+        # the panel will be drawed 4 times in a second
         self.speed = None
+        # length of the line
         self.length = None
+        # cycles number 
         self.number = None
-        
+    
+    # initialize the parameters   
     def initParas(self):
         self.setColor(['red','blue'])
         self.setParas(35,50,100,4)
 
         return
+    # set the color for 
+    # input: colors, array with two items
+    # such as : ["red","red"]
     def setColor(self,colors):
         turtle.color(colors[0],colors[1])
 
         return 
+    # set the rotation angle
     def setTheta(self,theta):
         self.theta = theta
         return 
 
+    # set the length of the line
     def setLength(self,length):
         self.length = length
         return
+    # set the number of cycles
     def setNumber(self,number):
         self.number = number
         return
 
+    # set the fps(frames per second)
     def setSpeed(self,speed):
         self.speed = speed
         turtle.speed(speed)
         return
 
+    # draw the pattern
     def draw(self):
         i = 0
         while i < self.number:
@@ -63,7 +78,9 @@ class TurtlePlay():
             turtle.right(self.theta)
             i += 1
 
-
+    # write the image into ps and pdf
+    # ps2pdf is a linux command which 
+    # converts a .ps file into a .pdf one
     def writeImage(self):    
 
         cv = turtle.getcanvas()
@@ -84,6 +101,7 @@ class TurtlePlay():
             turtle.right(self.theta)
 
         return
+        
     # draw a polygon
     def polygon(self,n):
         for i in range(n):
@@ -93,7 +111,8 @@ class TurtlePlay():
 
         return
 
-    def drawSquare(self):
+    # draw squares
+    def drawSquares(self):
         for i in range(self.number):
             self.square()
             turtle.right(5)
@@ -101,7 +120,8 @@ class TurtlePlay():
 
         return
 
-    def drawPolygon(self,n):
+    # draw polygons 
+    def drawPolygons(self,n):
         for i in range(self.number):
             self.polygon(n)
             turtle.right(5)
@@ -109,6 +129,7 @@ class TurtlePlay():
 
         return
 
+    # set all the parameters
     def setParas(self,theta,length,number,speed):
         self.setTheta(theta)
         self.setLength(length)
@@ -116,7 +137,8 @@ class TurtlePlay():
         self.setSpeed(speed)
 
         return 
-
+    
+    # print out all the parameters
     def printParas(self):
         print("theta: ",self.theta)
         print("length: ",self.length)
@@ -124,18 +146,20 @@ class TurtlePlay():
         print("speed: ",self.speed)
 
         return
-        
+    
+    # test for drawing the squares  
     def test(self):
         self.setColor(['red','blue'])
         self.setParas(90, 50, 60, 8)
         # self.draw()
-        self.drawSquare()
+        self.drawSquares()
         self.writeImage()
     
+    # test for drawing the polygons
     def testPolygon(self):
         n = 5
         theta = 360/n
         self.setColor(['red','blue'])
         self.setParas(theta, 50, 60, 16)
-        self.drawPolygon(n)
+        self.drawPolygons(n)
         self.writeImage()
