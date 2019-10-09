1264ec301dbd5dccb68f675f8c98744762958ee8
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-09 18:45:12 +0800

    add testMyCommon, getTestData and numbers2string in MyCommon.py

--- test/Test.py
+++ test/Test.py
@@ -16,6 +16,8 @@
 """
 
 import mytools
+import numpy as np
+
 class Test(object):
     """
     It is a class for testing the module mytools
@@ -40,12 +42,56 @@ class Test(object):
         test.interface()
 
         return
+    def testMyCommon(self):
+        """
+        docstring for testMyCommon
+        It is used to test the MyCommon class
+        """
+        test = mytools.MyCommon()
+        filename = "test.json"
+        data = self.getTestData()
+        test.writeFile(data,filename)
+        filename = filename.replace("json","yml")
+        test.writeFile(data,filename)
+
+        data = test.loadFile(filename)
+        print(data)
+
+        return
+    def getTestData(self):
+        """
+        docstring for getTestData
+        return: a dictionary with random string as 
+                keys,such as 
+                {"JCXJICNICK":[74,67,88,74,73,67,78,73,67,75]...}
+        """
+        data = {}
+        rand = np.random.randint(65,91,100)
+        rand = rand.reshape((10,10))
+
+        for value in rand:
+            key = self.numbers2string(value)
+            data[key] = value.tolist()
+
+        return data
+
+    def numbers2string(self,array):
+        """
+        docstring for numbers2string
+        [65,65,66] --> "AAB"
+        """
+        results = []
+        for i in array:
+            results.append(chr(i))
+        results = "".join(results)
+        return results
     def test(self):
         """
         test all the classes in the mytools module
         """
-        self.testDrawPig()
-        self.testMyGUI()
+        # self.testDrawPig()
+        # self.testMyGUI()
+        self.testMyCommon()
 
 test = Test()
 test.test()
