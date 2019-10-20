06456dc3d95d60b7eb8c6138ed9c390f4c8d5ec2
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-20 21:08:04 +0800

    add modules in __init__.py

---
+++ mycollections/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2019-10-17 21:04:21
+    @project      : my collections of python class
+    @version      : 1.0
+    @source file  : __init__.py
+
+============================
+"""
+
+from .CrawlRecipes import CrawlRecipes
+from .Manikon      import Manikon
+from .SlurmState   import SlurmState
+from .Ris2Bib      import Ris2Bib
+from .QTable       import QTable
