b04b5c479d60d6bfe55dbe013bdf227ee8ba7683
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-21 13:26:35 +0800

    add 05_treeMaker/02_getValue/03_v3

--- mycollections/Triangle/triangle.py
+++ mycollections/Triangle/triangle.py
@@ -35,9 +35,9 @@ class Triangle():
         lateral_AB = self.get_lateral_AB()
         lateral_AC = self.get_lateral_AC()
         lateral_BC = self.get_lateral_BC()
-        laterals["AB"] = (lateral_AB)
-        laterals["AC"] = (lateral_AC)
-        laterals["BC"] = (lateral_BC)
+        laterals["AB"] = lateral_AB
+        laterals["AC"] = lateral_AC
+        laterals["BC"] = lateral_BC
         
         return laterals
     def get_angle(self,length_a,length_b,length_c):
@@ -84,4 +84,65 @@ class Triangle():
         angles["angle C"] = angle_C
 
         return angles
+    def get_line(self,coor1,coor2):
+        # a*x + b*y + c = 0
+        a = coor2[1] - coor1[1]
+        b = coor1[0] - coor2[0]
+        c = -(a*coor1[0] + b*coor1[1])
+        return [a,b,c]
+    def get_line_AB(self):
+        return self.get_line(self.A,self.B)
+    def get_line_AC(self):
+        return self.get_line(self.A,self.C)
+    def get_line_BC(self):
+        return self.get_line(self.B,self.C)
+    def get_lines(self):
+        line_AB = self.get_line_AB()
+        line_AC = self.get_line_AC()
+        line_BC = self.get_line_BC()
+
+        lines = {}
+        lines["line AB"] = line_AB
+        lines["line AC"] = line_AC
+        lines["line BC"] = line_BC
+        return lines
+    def get_line_string(self,line):
+        # inverse the line
+        if line[0] < 0:
+            for i in range(3):
+                line[i] = - line[i]
+
+        values = [abs(line[0]),abs(line[1]),abs(line[2])]
+        string1 = "%.2fx + %.2fy + %.2f = 0"%(values[0],values[1],values[2])
+        string2 = "%.2fx + %.2fy - %.2f = 0"%(values[0],values[1],values[2])
+        string3 = "%.2fx - %.2fy + %.2f = 0"%(values[0],values[1],values[2])
+        string4 = "%.2fx - %.2fy - %.2f = 0"%(values[0],values[1],values[2])
+        if line[1] > 0:
+            if line[2] > 0:
+                return string1
+            else:
+                return string2
+        else:
+            if line[2] > 0:
+                return string3
+            else:
+                return string4
+    def get_line_string_AB(self):
+        line_AB = self.get_line_AB()
+        return self.get_line_string(line_AB)
+    def get_line_string_AC(self):
+        line_AC = self.get_line_AC()
+        return self.get_line_string(line_AC)
+    def get_line_string_BC(self):
+        line_BC = self.get_line_BC()
+        return self.get_line_string(line_BC)
+    def get_line_strings(self):
+        line_AB = self.get_line_string_AB()
+        line_AC = self.get_line_string_AC()
+        line_BC = self.get_line_string_BC()
+        lines = {}
+        lines["line AB"] = line_AB
+        lines["line AC"] = line_AC
+        lines["line BC"] = line_BC
+        return lines
 
