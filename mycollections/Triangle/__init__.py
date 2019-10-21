4e3f5ff3e189904826e4abfcef4122df4a418246
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-21 13:32:23 +0800

    add __init__.py in mycollections.Triangle

---
+++ mycollections/Triangle/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2019-10-21 13:31:41
+    @project      : Triangle project
+    @version      : 1.0
+    @source file  : __init__.py
+
+============================
+"""
+
+from .TriBase import TriBase
+from .Triangle import Triangle
