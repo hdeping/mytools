3cc929c6dd581a73d9540501913eb47c2e86f8de
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-21 19:30:22 +0800

    rename some methods

--- ml/MaterialGene/Wyckoff.py
+++ ml/MaterialGene/Wyckoff.py
@@ -22,7 +22,20 @@ class Wyckoff():
     """Docstring for Wyckoff. """
 
     def __init__(self):
-        """TODO: to be defined1. """
+        """TODO: to be defined1. 
+        self.codeDir:
+            directory of my codes for scientific research
+        self.dirs:
+            directory of the data
+        self.lattice:
+            lattice of the alloy
+        self.wyckoff_site:
+            from 0 to 72
+            [0,2, 6, 8, 20, 44, 56, 60, 64, 66, 70, 72]
+        self.wyckoff_type:
+            types for wyckoff sites
+            ['a', 'e', 'b', 'i', 'l', 'j', 'f', 'g', 'd', 'h', 'c']
+        """
 
         super(Wyckoff,self).__init__()
         self.codeDir = "/home/hdeping/complexNetwork/00_CCodes/"
@@ -40,20 +53,22 @@ class Wyckoff():
                              'd', 'h', 'c']
 
         return
-    def getFullMatrix1(self):
+    def getSiteMatrix(self,wyckoff_seq):
         assert  len(wyckoff_seq) == 11
         fullMatrix = np.zeros((3,3,8))
         # deal with every site in the wyckoff sequences
         for i,site in enumerate(wyckoff_seq):
             # in the range of wyckoff_site (i,i+1)
-            print(i,site)
-            for j in range(wyckoff_site[i],wyckoff_site[i+1]):
+            for j in range(self.wyckoff_site[i],self.wyckoff_site[i+1]):
                 # get the lattice position
                 ii,jj,kk = self.lattice[j]
                 fullMatrix[ii,jj,kk] = int(site)
     
         return fullMatrix
-    def test1(self):
+    def testSiteMatrix(self):
+        """
+        test for self.getFullMatrix
+        """
         count_i = np.zeros(3)
         count_j = np.zeros(3)
         count_k = np.zeros(8)
@@ -66,7 +81,7 @@ class Wyckoff():
         print(count_j)
         print(count_k)
         
-        fullMatrix = getFullMatrix(wyckoff_seq)
+        fullMatrix = self.getSiteMatrix(wyckoff_seq)
         print(fullMatrix)
         
         print(fullMatrix.shape)
@@ -75,6 +90,14 @@ class Wyckoff():
 
 
     def getFullMatrix(self):
+        """
+        get the full matrix, [11] --> [3,3,8]
+        input: 
+            None
+        return:
+            fullMatrix, labels, array type
+            [-1,3,3,8],[-1]
+        """
         filename = self.dirs + "01_3-3-8_194_energy.csv"
         data   = np.loadtxt(filename,delimiter=',',dtype=float)
     
@@ -85,7 +108,6 @@ class Wyckoff():
         assert  length == 11
         row = len(wyckoff_seq)
         fullMatrix = np.zeros((row,3,3,8))
-        #print(wyckoff_seq.shape)
         # deal with every site in the wyckoff sequences
     
         for i in range(length):
@@ -95,7 +117,10 @@ class Wyckoff():
     
             for j in range(wyckoff_site[i],wyckoff_site[i+1]):
                 # get the lattice position
-                ii,jj,kk = lattice[j]
+                ii,jj,kk = self.lattice[j]
                 fullMatrix[:,ii,jj,kk] = site
     
         return fullMatrix,labels
+
+site = Wyckoff()
+site.testSiteMatrix()
