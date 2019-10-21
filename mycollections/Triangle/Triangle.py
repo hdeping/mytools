2c427231303036b640975a4bb0b28bc57288bc0e
黄德平(Deping Huang) <xiaohengdao@gmail.com>
2019-10-21 13:40:46 +0800

    update Triangle.py

--- mycollections/Triangle/Triangle.py
+++ mycollections/Triangle/Triangle.py
@@ -1,11 +1,31 @@
 #!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+ 
+"""
 
-from base import base
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2019-10-21 13:32:30
+    @project      : Triangle project
+    @version      : 1.0
+    @source file  : Triangle.py
+
+============================
+"""
+
+
+from .TriBase import TriBase
 import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-class Triangle(base):
-    # init function
+class Triangle(TriBase):
+    """
+    all kinds of methods for triangle computation
+    """
     def __init__(self,points):
         self.A = points[0]
         self.B = points[1]
@@ -15,6 +35,8 @@ class Triangle(base):
     
     # get mid point of two points
     def get_mid_point(self,coor1,coor2):
+        """
+        """
         x = (coor1[0] + coor2[0])/2
         y = (coor1[1] + coor2[1])/2
         res = [x,y]
@@ -22,18 +44,26 @@ class Triangle(base):
 
     # get mid point of AB
     def get_mid_point_AB(self):
+        """
+        """
         return self.get_mid_point(self.A,self.B)
 
     # get mid point of AC
     def get_mid_point_AC(self):
+        """
+        """
         return self.get_mid_point(self.A,self.C)
 
     # get mid point of BC
     def get_mid_point_BC(self):
+        """
+        """
         return self.get_mid_point(self.C,self.B)
 
     # mid point information of the triangle
     def get_mid_points(self):
+        """
+        """
         point_AB = self.get_mid_point_AB()
         point_AC = self.get_mid_point_AC()
         point_BC = self.get_mid_point_BC()
@@ -45,12 +75,16 @@ class Triangle(base):
 
     # get the gravity center
     def get_gravity_center(self):
+        """
+        """
         center = np.mean(self.points,axis = 0)
         center = list(center)
         return center
 
     # draw the mid lines
     def draw_mid_lines(self):
+        """
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -71,22 +105,30 @@ class Triangle(base):
 
     # get the mid line of A-BC
     def get_mid_line_A_BC(self):
+        """
+        """
         point_BC = self.get_mid_point_BC()
         return self.get_line(self.A,point_BC)
 
     # get the mid line of B-AC
     def get_mid_line_B_AC(self):
+        """
+        """
         point_AC = self.get_mid_point_AC()
         return self.get_line(self.B,point_AC)
 
     # get the mid line of C-AB
     def get_mid_line_C_AB(self):
+        """
+        """
         point_AB = self.get_mid_point_AB()
         return self.get_line(self.C,point_AB)
 
     # get the mid line information of the
     # triangle
     def get_mid_lines(self):
+        """
+        """
         line_A_BC = self.get_mid_line_A_BC()
         line_B_AC = self.get_mid_line_B_AC()
         line_C_AB = self.get_mid_line_C_AB()
@@ -99,22 +141,30 @@ class Triangle(base):
 
     # get the mid line equation format of C_AB
     def get_mid_line_string_C_AB(self):
+        """
+        """
         line = self.get_mid_line_C_AB()
         return self.get_line_string(line)
 
     # get the mid line equation format of B-AC
     def get_mid_line_string_B_AC(self):
+        """
+        """
         line = self.get_mid_line_B_AC()
         return self.get_line_string(line)
 
     # get the mid line equation format of A-BC
     def get_mid_line_string_A_BC(self):
+        """
+        """
         line = self.get_mid_line_A_BC()
         return self.get_line_string(line)
 
     # get the mid line equation format information
     # of the triangle
     def get_mid_line_strings(self):
+        """
+        """
         line_A_BC = self.get_mid_line_string_A_BC()
         line_B_AC = self.get_mid_line_string_B_AC()
         line_C_AB = self.get_mid_line_string_C_AB()
@@ -129,10 +179,14 @@ class Triangle(base):
     # matrix[0] matrix[1]
     # matrix[2] matrix[3]
     def get_determinant(self,matrix):
+        """
+        """
         return matrix[0]*matrix[3] - matrix[1]*matrix[2]
 
     # get the intersect point of two lines
     def get_intersect_point(self,line1,line2):
+        """
+        """
         # get A
         matrix = []
         matrix.append(line1[0])
@@ -167,6 +221,8 @@ class Triangle(base):
 
     # test the get_intersect_point function
     def test_get_intersect_point(self):
+        """
+        """
         midLines = self.get_mid_lines()
         line_A_BC = midLines["line A-BC"]
         line_B_AC = midLines["line B-AC"]
@@ -183,6 +239,8 @@ class Triangle(base):
     # get ortho line of a point and a line
     # the ortho line is perpendicular to the input line
     def get_ortho_line(self,line,point):
+        """
+        """
         res = []
         res.append(line[1])
         res.append(- line[0])
@@ -192,22 +250,30 @@ class Triangle(base):
 
     # get the ortho line of A-BC
     def get_ortho_line_A_BC(self):
+        """
+        """
         line = self.get_line_BC()
         return self.get_line(line,self.A)
 
     # get the ortho line of B-AC
     def get_ortho_line_B_AC(self):
+        """
+        """
         line = self.get_line_AC()
         return self.get_line(line,self.B)
 
     # get the ortho line of C-AB
     def get_ortho_line_C_AB(self):
+        """
+        """
         line = self.get_line_AB()
         return self.get_line(line,self.C)
 
     # get the ortho line information of the
     # triangle
     def get_ortho_lines(self):
+        """
+        """
         line_A_BC = self.get_ortho_line_A_BC()
         line_B_AC = self.get_ortho_line_B_AC()
         line_C_AB = self.get_ortho_line_C_AB()
@@ -220,22 +286,30 @@ class Triangle(base):
 
     # get the ortho line equation format of C_AB
     def get_ortho_line_string_C_AB(self):
+        """
+        """
         line = self.get_ortho_line_C_AB()
         return self.get_line_string(line)
 
     # get the ortho line equation format of B-AC
     def get_ortho_line_string_B_AC(self):
+        """
+        """
         line = self.get_ortho_line_B_AC()
         return self.get_line_string(line)
 
     # get the ortho line equation format of A-BC
     def get_ortho_line_string_A_BC(self):
+        """
+        """
         line = self.get_ortho_line_A_BC()
         return self.get_line_string(line)
 
     # get the ortho line equation format information
     # of the triangle
     def get_ortho_line_strings(self):
+        """
+        """
         line_A_BC = self.get_ortho_line_string_A_BC()
         line_B_AC = self.get_ortho_line_string_B_AC()
         line_C_AB = self.get_ortho_line_string_C_AB()
@@ -248,6 +322,8 @@ class Triangle(base):
 
     # get ortho point of a point and a line
     def get_ortho_point(self,line,point):
+        """
+        """
         ortho_line = self.get_ortho_line(line,point)
         #print(ortho_line)
         res = self.get_intersect_point(line,ortho_line)
@@ -255,21 +331,29 @@ class Triangle(base):
 
     # get ortho point of AB
     def get_ortho_point_C_AB(self):
+        """
+        """
         line = self.get_line_AB()
         return self.get_ortho_point(line,self.C)
 
     # get ortho point of AC
     def get_ortho_point_B_AC(self):
+        """
+        """
         line = self.get_line_AC()
         return self.get_ortho_point(line,self.B)
 
     # get ortho point of BC
     def get_ortho_point_A_BC(self):
+        """
+        """
         line = self.get_line_BC()
         return self.get_ortho_point(line,self.A)
 
     # ortho point information of the triangle
     def get_ortho_points(self):
+        """
+        """
         point_C_AB = self.get_ortho_point_C_AB()
         point_B_AC = self.get_ortho_point_B_AC()
         point_A_BC = self.get_ortho_point_A_BC()
@@ -281,6 +365,8 @@ class Triangle(base):
 
     # draw the orthogonal lines
     def draw_ortho_lines(self):
+        """
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -304,6 +390,8 @@ class Triangle(base):
     # the vertical line is perpendicular to the 
     # line constructed by the input points
     def get_vertical_line(self,point1,point2):
+        """
+        """
         mid_point = self.get_mid_point(point1,point2)
         line      = self.get_line(point1,point2)
         res       = self.get_ortho_line(line,mid_point)
@@ -311,19 +399,27 @@ class Triangle(base):
 
     # get the vertical line of BC
     def get_vertical_line_BC(self):
+        """
+        """
         return self.get_vertical_line(self.B,self.C)
 
     # get the vertical line of AC
     def get_vertical_line_AC(self):
+        """
+        """
         return self.get_vertical_line(self.C,self.A)
 
     # get the vertical line of AB
     def get_vertical_line_AB(self):
+        """
+        """
         return self.get_vertical_line(self.B,self.A)
 
     # get the vertical line information of the
     # triangle
     def get_vertical_lines(self):
+        """
+        """
         line_BC = self.get_vertical_line_BC()
         line_AC = self.get_vertical_line_AC()
         line_AB = self.get_vertical_line_AB()
@@ -336,22 +432,30 @@ class Triangle(base):
 
     # get the vertical line equation format of AB
     def get_vertical_line_string_AB(self):
+        """
+        """
         line = self.get_vertical_line_AB()
         return self.get_line_string(line)
 
     # get the vertical line equation format of AC
     def get_vertical_line_string_AC(self):
+        """
+        """
         line = self.get_vertical_line_AC()
         return self.get_line_string(line)
 
     # get the vertical line equation format of BC
     def get_vertical_line_string_BC(self):
+        """
+        """
         line = self.get_vertical_line_BC()
         return self.get_line_string(line)
 
     # get the vertical line equation format information
     # of the triangle
     def get_vertical_line_strings(self):
+        """
+        """
         line_BC = self.get_vertical_line_string_BC()
         line_AC = self.get_vertical_line_string_AC()
         line_AB = self.get_vertical_line_string_AB()
@@ -365,6 +469,8 @@ class Triangle(base):
     # get the center of the circum circle of the 
     # triangle
     def get_circum_center(self):
+        """
+        """
         lines = self.get_vertical_lines()
         line_BC = lines["line BC"] 
         line_AB = lines["line AB"] 
@@ -374,6 +480,8 @@ class Triangle(base):
     # get the ortho center of the 
     # triangle
     def get_ortho_center(self):
+        """
+        """
         lines = self.get_ortho_lines()
         line_BC = lines["line A-BC"] 
         line_AB = lines["line C-AB"] 
@@ -382,6 +490,8 @@ class Triangle(base):
 
     # draw the circum circle
     def draw_circum_circle(self):
+        """
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -398,6 +508,8 @@ class Triangle(base):
     
     # get side value of a line and a point
     def get_point_line_value(self,line,point):
+        """
+        """
         res = line[2]
         for i in range(2):
             res += line[i]*point[i]
@@ -407,6 +519,8 @@ class Triangle(base):
     # the bisector line is perpendicular to the 
     # line constructed by the input points
     def get_bisector_line(self,point1,point2,point3):
+        """
+        """
         line1 = self.get_line(point1,point2)
         line2 = self.get_line(point1,point3)
         
@@ -450,22 +564,30 @@ class Triangle(base):
 
     # get the inscribe line of A
     def get_inscribe_line_A(self):
+        """
+        """
         line = self.get_bisector_line(self.A,self.B,self.C)
         return line['interior']
 
     # get the inscribe line of B
     def get_inscribe_line_B(self):
+        """
+        """
         line = self.get_bisector_line(self.B,self.A,self.C)
         return line['interior']
 
     # get the inscribe line of C
     def get_inscribe_line_C(self):
+        """
+        """
         line =  self.get_bisector_line(self.C,self.A,self.B)
         return line['interior']
 
     # get the inscribe line information of the
     # triangle
     def get_inscribe_lines(self):
+        """
+        """
         line_A = self.get_inscribe_line_A()
         line_B = self.get_inscribe_line_B()
         line_C = self.get_inscribe_line_C()
@@ -478,22 +600,29 @@ class Triangle(base):
 
     # get the inscribe line equation format of A
     def get_inscribe_line_string_A(self):
+        """
+        """
         line = self.get_inscribe_line_A()
         return self.get_line_string(line)
 
     # get the inscribe line equation format of B
     def get_inscribe_line_string_B(self):
+        """
+        """
         line = self.get_inscribe_line_B()
         return self.get_line_string(line)
 
     # get the inscribe line equation format of C
     def get_inscribe_line_string_C(self):
+        """
+        """
         line = self.get_inscribe_line_C()
         return self.get_line_string(line)
 
     # get the inscribe line equation format information
     # of the triangle
-    def get_inscribe_line_strings(self):
+    def get_inscribe_line_strings(self):"""
+        """
         line_A = self.get_inscribe_line_string_A()
         line_B = self.get_inscribe_line_string_B()
         line_C = self.get_inscribe_line_string_C()
@@ -507,6 +636,8 @@ class Triangle(base):
     # get the center of the inscribed circle of the 
     # triangle
     def get_inscribe_center(self):
+        """
+        """
         lines = self.get_inscribe_lines()
         line_B = lines["B"] 
         line_A = lines["A"] 
@@ -516,6 +647,8 @@ class Triangle(base):
 
     # draw the inscribe circle
     def draw_inscribe_circle(self):
+        """
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -542,22 +675,31 @@ class Triangle(base):
 
     # get the escribe line of A
     def get_escribe_line_A(self):
+        """
+        """
         line = self.get_bisector_line(self.A,self.B,self.C)
         return line['exterior']
 
     # get the escribe line of B
+
     def get_escribe_line_B(self):
+        """
+        """
         line = self.get_bisector_line(self.B,self.A,self.C)
         return line['exterior']
 
     # get the escribe line of C
     def get_escribe_line_C(self):
+        """
+        """
         line =  self.get_bisector_line(self.C,self.A,self.B)
         return line['exterior']
 
     # get the escribe line information of the
     # triangle
     def get_escribe_lines(self):
+        """
+        """
         line_A = self.get_escribe_line_A()
         line_B = self.get_escribe_line_B()
         line_C = self.get_escribe_line_C()
@@ -570,22 +712,30 @@ class Triangle(base):
 
     # get the escribe line equation format of A
     def get_escribe_line_string_A(self):
+        """
+        """
         line = self.get_escribe_line_A()
         return self.get_line_string(line)
 
     # get the escribe line equation format of B
     def get_escribe_line_string_B(self):
+        """
+        """
         line = self.get_escribe_line_B()
         return self.get_line_string(line)
 
     # get the escribe line equation format of C
     def get_escribe_line_string_C(self):
+        """
+        """
         line = self.get_escribe_line_C()
         return self.get_line_string(line)
 
     # get the escribe line equation format information
     # of the triangle
     def get_escribe_line_strings(self):
+        """
+        """
         line_A = self.get_escribe_line_string_A()
         line_B = self.get_escribe_line_string_B()
         line_C = self.get_escribe_line_string_C()
@@ -599,6 +749,8 @@ class Triangle(base):
     # get the center of the escribed circle A-BC of the 
     # triangle
     def get_escribe_center_A_BC(self):
+        """
+        """
         lines = self.get_escribe_lines()
         line_B = lines["B"] 
         line_C = lines["C"] 
@@ -608,6 +760,8 @@ class Triangle(base):
     # get the center of the escribed circle B-AC of the 
     # triangle
     def get_escribe_center_B_AC(self):
+        """
+        """
         lines = self.get_escribe_lines()
         line_A = lines["A"] 
         line_C = lines["C"] 
@@ -617,6 +771,8 @@ class Triangle(base):
     # get the center of the escribed circle C-AB of the 
     # triangle
     def get_escribe_center_C_AB(self):
+        """
+        """
         lines = self.get_escribe_lines()
         line_A = lines["A"] 
         line_B = lines["B"] 
@@ -625,6 +781,8 @@ class Triangle(base):
 
     # draw the circum circle
     def draw_escribe_circle(self):
+        """
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -644,6 +802,8 @@ class Triangle(base):
     # inscribe center
     # 3 escribe centers
     def get_centers(self):
+        """
+        """
         gravity_center = self.get_gravity_center()
         circum_center = self.get_circum_center()
         ortho_center = self.get_ortho_center()
@@ -663,8 +823,10 @@ class Triangle(base):
 
         return centers
     
-    # get all the information of the triangle
     def get_info(self):
+        """
+        get all the information of the triangle
+        """
         vertices       = self.get_vertices()
         side_lengths   = self.get_laterals()
         angles         = self.get_angles()
@@ -694,9 +856,11 @@ class Triangle(base):
 
         return res
 
-    # extend the line to three times of itself
-    # get the two end points
     def get_extend_points(self,point1,point2):
+        """
+        extend the line to three times of itself
+        get the two end points
+        """
         res1 = []
         res2 = []
 
@@ -708,8 +872,10 @@ class Triangle(base):
         
         return res1,res2
 
-    # draw all the circles and some lines
     def draw_inscribe_circle(self):
+        """
+        draw all the circles and some lines
+        """
         plt.figure(figsize = (9,9))
         plt.axis("equal")
 
@@ -764,7 +930,7 @@ class Triangle(base):
     def run(self):
         """TODO: Docstring for run.
         :returns: TODO
-
+        entry for the class Triangle
         """
         points = [[3.0,0],[0.0,4.0],[0.0,0.0]]
         #points = [[4.0,0],[0.0,4.0],[0.0,0.0]]
