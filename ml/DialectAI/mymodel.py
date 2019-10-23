b04a510185b77dbe903b0dfcf0fdbdcc9e44fc5e
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:21 +0800

    add 00_before/91_fb40_SRU_twoLayer

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -17,11 +17,11 @@ class LanNet(nn.Module):
         self.layer1 = nn.Sequential()
         self.layer1.add_module('sru', 
                 SRU(self.input_dim, self.hidden_dim, 
-                    num_layers=1, 
+                    num_layers=2, 
                     bidirectional=False,
-                    use_tanh=0,
+                    use_tanh=1,
                     use_relu=0,
-                    use_selu=1,
+                    use_selu=0,
                     weight_norm=False,
                     layer_norm = False,
                     highway_bias = 0))
