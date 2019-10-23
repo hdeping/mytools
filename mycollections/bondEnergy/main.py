f7b948ac95ac76ebad6abd3c98a26917b8bc4a45
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:13 +0800

    add 060_getCorrentNumber

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -7,7 +7,6 @@ import numpy as np
 
 obConversion = ob.OBConversion()
 obConversion.SetInAndOutFormats("smi", "smi")
-#obFP = ob.OBFingerprint_Tanimoto()
 vec = ob.OBBitVec()
 
 def readSMILES(smi_string):
@@ -22,4 +21,13 @@ string1 = "[CH]OC(CC(=O)O)(CC(=O)O)O"
 string2 = "[C](=O)C[C@@](CC(=O)O)(O)OC=O"
 mol1 = readSMILES(string1)
 mol2 = readSMILES(string2)
-tanimoto = ob.OBFingerprint.Tanimoto(mol1,mol2)
+
+name = "FP2"
+fingerprinter = ob.OBFingerprint.FindFingerprint(name)
+vec1 = ob.vectorUnsignedInt()
+vec2 = ob.vectorUnsignedInt()
+fingerprinter.GetFingerprint(mol1, vec1)
+fingerprinter.GetFingerprint(mol2, vec2)
+tanimoto_value = fingerprinter.Tanimoto(vec1,vec2)
+print(len(vec1))
+print(tanimoto_value)
