208f096031aa1109207deef3ec93656e32984085
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:22 +0800

    add 188_tenGru_maskBefore_all_majorityVote

--- ml/DialectAI/analyze.py
+++ ml/DialectAI/analyze.py
@@ -1,24 +1,22 @@
 import numpy as np
 
 data = np.loadtxt('result.txt')
-#print(data)
+print(data)
 
 data = data.astype(int)
-print(data.shape)
-data = np.reshape(data,(1000,-1,2))
-num = 2
+data = np.reshape(data,(-1,20,2))
+num = 10
 stati = np.zeros((num,num))
 stati = stati.astype(int)
 size = len(data)
 # get the majorityVote
 target = data[:,0,0]
 majority = []
-print(len(data[0]))
 for i in range(size):
     # get the vote 
-    vote = np.zeros(num)
+    vote = np.zeros(20)
     vote = vote.astype(int)
-    for j in range(len(data[0])):
+    for j in range(20):
         ii = data[i,j,1]
         vote[ii] += 1
     # append the vote result
@@ -27,11 +25,6 @@ for i in range(size):
     ii = target[i]
     jj = majority[i]
     stati[ii,jj] += 1
-arr1 = np.sum(stati,axis=1)
-arr2 = np.sum(stati,axis=0)
-arr = [stati[i,i] for i in range(num)]
+
+
 print(stati)
-print("precision , recall")
-print(arr/arr1)
-print(arr/arr2)
-print(sum(arr))
