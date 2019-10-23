351c3dc91070d67c76558b4345639e14e9f31185
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:22 +0800

    add 181_KejiaAndMinnan_threeGru_res

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -13,11 +13,11 @@ class LanNet(nn.Module):
         self.output_dim = output_dim
 
         self.layer0 = nn.Sequential()
-        self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=2, dropout=0.1,batch_first=True, bidirectional=False))
+        self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layer1 = nn.Sequential()
-        self.layer1.add_module('gru', nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=2, dropout=0.1,batch_first=True, bidirectional=False))
+        self.layer1.add_module('gru', nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layera = nn.Sequential()
-        self.layera.add_module('gru', nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=2, dropout=0.1,batch_first=True, bidirectional=False))
+        self.layera.add_module('gru', nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
 
         self.layer2 = nn.Sequential()
         self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
@@ -33,8 +33,10 @@ class LanNet(nn.Module):
 
         # get gru output
         out_hidden, hidd = self.layer0(src)
-        out_hidden, hidd = self.layer1(out_hidden)
-        out_hidden, hidd = self.layera(out_hidden)
+        out_hidden1, hidd = self.layer1(out_hidden)
+        out_hidden2, hidd = self.layera(out_hidden1)
+        # resblock
+        out_hidden = out_hidden1 + out_hidden2
         # summation of the two hidden states in the same node
         # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
         #print(out_hidden.shape)
