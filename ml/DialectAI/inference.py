c26720d126c810f669829e680592b20a767dddb0
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:23 +0800

    add 333_ten_CTCLoss4_resnet_twowoGRURes_all_double

--- ml/DialectAI/inference.py
+++ ml/DialectAI/inference.py
@@ -31,18 +31,19 @@ import torch.utils.data as Data
 
 #from read_data import get_samples, get_data, TorchDataSet
 from mydata import  TorchDataSet
-from testmodel import LanNet,BiRes
+from testmodel import LanNet
 
 ## ======================================
 # data list
 # train
-dev_list   = "../labels/label_dev_list_fb.txt"
+dev_list   = "label_dev_list_fb.txt"
 
 # basic configuration parameter
 use_cuda = torch.cuda.is_available()
 # network parameter 
 toneLengthD = 6
 dimension = 40
+data_dimension = 320
 language_nums = 10 # 9!
 learning_rate = 0.1
 batch_size = 50
@@ -51,10 +52,17 @@ chunk_num = 10
 train_iteration = 16
 display_fre = 50
 half = 4
+# data augmentation
 
-hidden_dim = 512
-bn_dim     = 64
+# save the models
+import sys
+#model_dir = "models" + sys.argv[1]
+model_dir = "models"
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
 
+# seed 
+torch.manual_seed(time.time())
 
 ## ======================================
 # with data augmentation
@@ -62,9 +70,26 @@ bn_dim     = 64
 dev_dataset = TorchDataSet(dev_list, batch_size, chunk_num, dimension)
 logging.info('finish reading all train data')
 
+# 优化器，SGD更新梯度
+train_module = LanNet(input_dim=dimension, hidden_dim=128, bn_dim=30, output_dim=language_nums)
+logging.info(train_module)
+optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
 
+# initialize the model
+#device = torch.device("cuda:2")
+# 将模型放入GPU中
+if use_cuda:
+    # torch 0.4.0
+    #train_module = train_module.to(device)
+    # torch 0.3.0
+    train_module = train_module.cuda()
 
-def test1(train_module):
+# regularization factor
+factor = 0.0005
+
+##  -----------------------------------------------------------------------------------------------------------------------------
+##  dev
+def test():
     train_module.eval()
     epoch_tic = time.time()
     dev_loss = 0.
@@ -73,25 +98,36 @@ def test1(train_module):
     
     result_target = []
     for step, (batch_x, batch_y) in enumerate(dev_dataset): 
-        print("step is ",step)
+        #print("step is ",step)
         tic = time.time()
     
         batch_target = batch_y[:,0].contiguous().view(-1, 1).long()
-        batch_frames = batch_y[:,1].contiguous().view(-1, 1).long()
+        batch_frames = batch_y[:,1].contiguous().view(-1, 1)
     
         max_batch_frames = int(max(batch_frames).item())
         batch_dev_data = batch_x[:, :max_batch_frames, :]
     
         step_batch_size = batch_target.size(0)
+        batch_mask = torch.zeros(step_batch_size, max_batch_frames)
+        for ii in range(step_batch_size):
+            frames = int(batch_frames[ii].item())
+            batch_mask[ii, :frames] = 1.
     
         # 将数据放入GPU中
         if use_cuda:
+            # torch 0.4.0
+            #batch_dev_data   = batch_dev_data.to(device)
+            #batch_mask       = batch_mask.to(device)
+            #batch_target     = batch_target.to(device)
+            # torch 0.3.0
             batch_dev_data   = batch_dev_data.cuda()
-            batch_frames       = batch_frames.cuda()
+            batch_mask       = batch_mask.cuda()
             batch_target     = batch_target.cuda()
             
         with torch.no_grad():
-            acc, loss,prediction = train_module(batch_dev_data, batch_frames, batch_target)
+            #acc, loss = train_module(batch_dev_data, batch_mask, batch_target)
+            acc, loss,prediction = train_module(batch_dev_data, batch_mask, batch_target)
+        #print(batch_target,prediction)
         for i in range(batch_size):
             result_target.append([batch_target[i].item(),prediction[i].item()])
             #result_target.append(prediction[i].item())
@@ -110,107 +146,24 @@ def test1(train_module):
     acc=dev_acc/dev_batch_num
     logging.info('dev-acc:%.6f, dev-loss:%.6f, cost time :%.6fs', acc, dev_loss/dev_batch_num, epoch_time)
     return result_target
-def test2(train_module):
-    train_module.eval()
-    epoch_tic = time.time()
-    dev_loss = 0.
-    dev_acc = 0.
-    dev_batch_num = 0 
-    
-    result_target = []
-    for step, (batch_x, batch_y) in enumerate(dev_dataset): 
-        print("step is ",step)
-        tic = time.time()
-    
-        batch_target = batch_y[:,0].contiguous().view(-1, 1).long()
-        batch_frames = batch_y[:,1].contiguous().view(-1, 1).long()
-    
-        max_batch_frames = int(max(batch_frames).item())
-        batch_dev_data = batch_x[:, :max_batch_frames, :]
-    
-        step_batch_size = batch_target.size(0)
-    
-        # 将数据放入GPU中
-        if use_cuda:
-            batch_dev_data   = batch_dev_data.cuda()
-            batch_frames       = batch_frames.cuda()
-            batch_target     = batch_target.cuda()
-            
-        with torch.no_grad():
-            acc, loss,prediction,data = train_module(batch_dev_data, batch_frames, batch_target)
-        for i in range(batch_size):
-            result_target.append([prediction[i].item(),data[i].item()])
-        
-        loss = loss.sum()/step_batch_size
-    
-        toc = time.time()
-        step_time = toc-tic
-    
-        dev_loss += loss.item()
-        dev_acc += acc
-        dev_batch_num += 1
-    
-    epoch_toc = time.time()
-    epoch_time = epoch_toc-epoch_tic
-    acc=dev_acc/dev_batch_num
-    logging.info('dev-acc:%.6f, dev-loss:%.6f, cost time :%.6fs', acc, dev_loss/dev_batch_num, epoch_time)
-    return result_target
 # output the result
-# res_target: (N,2)   label    and predicted label
-# target_prop: (N,2)  predicted and probability
-def getNewTarget(res_target,target_prop):
-    # select the language with the label 3,6,8
-    # changsha,hebei,sichuan
-    res_target = torch.IntTensor(res_target)
-    select = torch.randn(len(res_target))
-    for i,line in enumerate(target_prop):
-        if line[0] == 3 or line[0] == 6 or line[0] == 8:
-            select[i] = line[1]
-        else:
-            select[i] = 0
-    # sort the select in a descending order
-    
-    data,sorted_indeces = torch.sort(select,descending=True)
-
-    #print(data[:10],sorted_indeces[:10])
-    # select previous 1500 ones
-    sorted_indeces = sorted_indeces[:1500]
-    for order in sorted_indeces:
-        # replace the target
-        print(order)
-        res_target[order,1] = target_prop[order][0]
-
-    # return res_target
-    return res_target
-
-
 import numpy as np
-# model 1
-train_module = LanNet(input_dim=dimension, hidden_dim=hidden_dim, bn_dim=bn_dim, output_dim=language_nums)
-logging.info(train_module)
+result = []
+for i in range(6):
+    print("model ",i)
+    print("loading model9-%d.model"%(i))
+    train_module.load_state_dict(torch.load("models/model9-%d.model"%(i)))
+    result_target = test()
+    result.append(result_target)
 
-# 将模型放入GPU中
-if use_cuda:
-    train_module = train_module.cuda()
-train_module.load_state_dict(torch.load("infer/infer.model"))
-result_target1 = test1(train_module)
-
-# model 2
-train_module = BiRes(input_dim=dimension, hidden_dim=128, bn_dim=30, output_dim=language_nums)
-logging.info(train_module)
-
-# 将模型放入GPU中
-if use_cuda:
-    train_module = train_module.cuda()
-train_module.load_state_dict(torch.load("infer/bi-res.model"))
-result_target2 = test2(train_module)
-
-
-target = getNewTarget(result_target1,result_target2)
-correct = 0
-for line in target:
-    if line[0] == line[1]:
-        correct = correct + 1
-
-print("correct = ",correct)
-
+# deal with the output
+result = np.array(result)
+result = np.transpose(result,(1,0,2))
+print(result.shape)
+size = len(result)
+#new = np.zeros((5000,2*size))
+#new[:,0] = result[:,0,0]
+#new[:,1:] = result[:,:,1]
+result = np.reshape(result,(size,-1))
+print(result.shape)
+np.savetxt("result.txt",result,fmt='%d')
--- ml/DialectAI/testmodel.py
+++ ml/DialectAI/testmodel.py
@@ -3,13 +3,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.utils.rnn import pack_padded_sequence,pad_packed_sequence
-import numpy as np
-
-
-from resnet import resnet18
-
-
 
 class LanNet(nn.Module):
     def __init__(self, input_dim=48, hidden_dim=2048, bn_dim=100, output_dim=10):
@@ -19,99 +12,10 @@ class LanNet(nn.Module):
         self.bn_dim = bn_dim
         self.output_dim = output_dim
 
-        self.conv = resnet18()
-
-        self.layer_gru = nn.Sequential()
-        self.layer_gru.add_module('gru', nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True))
-
-        self.layer1 = nn.Sequential()
-        self.layer1.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
-        self.layer1.add_module('linear', nn.Linear(self.hidden_dim, self.bn_dim))
-
-        self.layer2 = nn.Sequential()
-        self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.bn_dim))
-        self.layer2.add_module('linear', nn.Linear(self.bn_dim, self.output_dim))
-
-    def getBiHidden(self,layer,src,frames):
-        # pack the sequence
-        src = pack_padded_sequence(src,frames,batch_first=True)
-        # get the gru output
-        out_hidden, hidd = layer(src)
-        # unpack the sequence
-        out_hidden,lengths = pad_packed_sequence(out_hidden,batch_first=True)
-        # add the forward-backward value
-        out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
-        return out_hidden
-
-    def forward(self, x, frames,target):
-        batch_size, fea_frames, fea_dim = x.size()
-        # squeeze frames:  [batch_size,1] --> [batch_size]
-        frames = frames.squeeze()
-        # get packed sequence
-        sorted_frames,sorted_indeces = torch.sort(frames,descending=True)
-        # new input 
-        x = x[sorted_indeces]
-        # conv output
-        # new target
-        target = target[sorted_indeces]
-
-        x = x.unsqueeze(1)
-        x = self.conv(x)
-
-        # squeeze
-        # B,F,T -> B,T,F
-        x = x.squeeze()
-        x = x.transpose(1,2)
-
-        sorted_frames = sorted_frames / 4
-
-        new_indeces,older_indeces = torch.sort(sorted_indeces)
-
-        batch_size, time_frame ,hidden_dim = x.size()
-        # gru output
-        # layer gru
-        out_hidden = self.getBiHidden(self.layer_gru,x,sorted_frames)
-        # get a vector with fixed size (hidden_dim)
-        sorted_frames = sorted_frames.view(-1,1)
-        sorted_frames = sorted_frames.expand(batch_size,out_hidden.size(2))
-        sorted_frames = sorted_frames.type(torch.cuda.FloatTensor)
-
-        out_hidden = out_hidden.sum(dim=1)/sorted_frames
-
-        x = out_hidden
-        # target should be ordered
-        out_bn = self.layer1(x)
-        out_target = self.layer2(out_bn)
-        # softmax
-        predict_target = F.softmax(out_target, dim=1)
-
-        # 计算loss
-        tar_select_new = torch.gather(predict_target, 1, target)
-        ce_loss = -torch.log(tar_select_new) 
-        ce_loss = ce_loss.sum() / batch_size
-
-        # 计算acc
-        (data, predict) = predict_target.max(dim=1)
-        prediction = predict[older_indeces]
-        predict = predict.contiguous().view(-1,1)
-        correct = predict.eq(target).float()       
-        num_samples = predict.size(0)
-        sum_acc = correct.sum().item()
-        acc = sum_acc/num_samples
-
-        return acc, ce_loss, prediction
-class BiRes(nn.Module):
-    def __init__(self, input_dim=48, hidden_dim=2048, bn_dim=100, output_dim=10):
-        super(BiRes, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.bn_dim = bn_dim
-        self.output_dim = output_dim
-
         #self.layer0 = nn.Sequential()
         #self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layer1 = nn.Sequential()
-        self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True))
+        self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
 
         self.layer2 = nn.Sequential()
         self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
@@ -122,41 +26,19 @@ class BiRes(nn.Module):
         self.layer3.add_module('batchnorm', nn.BatchNorm1d(self.bn_dim))
         self.layer3.add_module('linear', nn.Linear(self.bn_dim, self.output_dim))
 
-    #def forward(self, src, mask, target):
-    def forward(self, src, frames, target):
+    def forward(self, src, mask, target):
         batch_size, fea_frames, fea_dim = src.size()
-        # squeeze frames:  [batch_size,1] --> [batch_size]
-        frames = frames.squeeze()
-        # get packed sequence
-        sorted_frames,sorted_indeces = torch.sort(frames,descending=True)
-        #print(sorted_frames)
-        #print(sorted_frames.shape)
-        #print(sorted_indeces.shape)
-        # new input 
-        src = src[sorted_indeces]
-        src = pack_padded_sequence(src,sorted_frames.cpu().numpy(),batch_first=True)
-        # new target
-        target = target[sorted_indeces]
-
 
         # get gru output
         out_hidden, hidd = self.layer1(src)
-        out_hidden,lengths = pad_packed_sequence(out_hidden,batch_first=True)
-
         # summation of the two hidden states in the same node
         # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
-        #mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_hidden.size(2))
+        #print(out_hidden.shape)
+        # get  masked outputs
+        mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_hidden.size(2))
         # output with new size (batch_size, hidden_dim)
-        #out_hidden = out_hidden*mask
-        # get a vector with fixed size length 
-        sorted_frames = sorted_frames.view(-1,1)
-        sorted_frames = sorted_frames.expand(batch_size,out_hidden.size(2))
-        sorted_frames = sorted_frames.type(torch.cuda.FloatTensor)
-        #print(sorted_frames)
-        out_hidden = out_hidden.sum(dim=1)/sorted_frames
-
-        out_hidden = out_hidden[:,0:self.hidden_dim] + out_hidden[:,self.hidden_dim:]
-        # linear parts
+        out_hidden = out_hidden*mask
+        out_hidden = out_hidden.sum(dim=1)/mask.sum(dim=1)
         #out_hidden = out_hidden.contiguous().view(-1, out_hidden.size(-1))   
         out_bn = self.layer2(out_hidden)
         out_target = self.layer3(out_bn)
@@ -176,13 +58,11 @@ class BiRes(nn.Module):
 
         # 计算acc
         (data, predict) = predict_target.max(dim=1)
-        new_indeces , indeces = torch.sort(sorted_indeces)
-        prediction = predict[indeces]
-        #prediction = predict
+        prediction = predict
         predict = predict.contiguous().view(-1,1)
         correct = predict.eq(target).float()       
         num_samples = predict.size(0)
         sum_acc = correct.sum().item()
         acc = sum_acc/num_samples
 
-        return acc, ce_loss, prediction ,data
+        return acc, ce_loss,prediction
