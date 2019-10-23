8bb765e966649ba1e3d83893aa3777a6ebba992e
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:12 +0800

    add 030_getJson7

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -1,58 +1,125 @@
 #!/usr/bin/python
 
-
-import openbabel as ob
 import numpy as np
+import json
+import sys
 
-obConversion = ob.OBConversion()
-obConversion.SetInAndOutFormats("smi", "smi")
-mol = ob.OBMol()
+filename1 = sys.argv[1] 
+filenames = np.loadtxt(filename1,dtype=str)
+suffix = '_frag00.out'
 
-def readSMILES(smi_string):
-    # read smiles
-    obConversion.ReadString(mol, smi_string)
-    # get bonds
-    MolBond = []
-    ChainBond = []
-    
-    for angle in ob.OBMolAngleIter(mol):    
-        print(angle)
-    
-    for bond in ob.OBMolBondIter(mol):    
-        a = bond.GetBeginAtomIdx()
-        b = bond.GetEndAtomIdx()
-        bo = bond.GetBondOrder()
-        MolBond.append((a,b,bo))
-    
-        if bond.IsInRing() or bond.IsAromatic():
-            continue
-    
-        ChainBond.append(bond.GetIdx())
-    
-    print(MolBond)
-    #MolBond.sort()
-    #print(MolBond)
-    #NumOfRingBond = len(MolBond) - len(ChainBond)
-def getSMILES(filename):
-    fp = open(filename,'r')
-    data = fp.read()
-    fp.close()
-    data = data.split('\n')
-    data = data[:-1]
-    res = []
-    # get the first column
-    for line in data:
-        line = line.split(' ')
-        res.append(line[0])
+
+def getPara(ii):
+    filename = "../data/%s%s.para"%(filenames[ii],suffix)
+    data = np.loadtxt(filename,delimiter=' ',dtype=str)
+
+    numberR = 0
+    numberA = 0
+    # order: R,A,D
+    for line in data[:,0]:
+        first_letter = line[0]
+        if first_letter == "R":
+            numberR += 1
+        elif first_letter == "A":
+            numberA += 1
+
+    # bonds,angles,dihedral
+    bonds    = data[:numberR]
+    angles   = data[numberR:numberR+numberA]
+    dihedral = data[numberR+numberA:]
+
+    return bonds,angles,dihedral
+
+def checkPara():
+    size = len(filenames)
+    count = 0
+    for i in range(size):
+        data = getPara(i)
+        # if the first letter is "R","A" or "D"
+        letters = ["R","A","D"]
+        #print(data[:,0])
+        for line in data[:,0]:
+            first_letter = line[0]
+            #print(first_letter)
+            if first_letter not in letters :
+                count = count + 1
+                print(i,first_letter)
+                break
+
+        #break
+
+    print("there are %d wrong samples"%(count))
+
+def getSerial(ii):
+    filename = "../data/%s%s.serial"%(filenames[ii],suffix)
+    data = np.loadtxt(filename,delimiter=' ',dtype=str)
+    return data
+
+def arrToDicts(arr):
+    res = {}
+    # arr to dicts
+
+    for line in arr:
+        key = line[1]
+        value = float(line[2])
+        res[key]  = value
 
     return res
+def getSample(ii):
+    result = {}
+    # get atoms
+    serial = getSerial(ii)
+    result["atoms"] = list(serial[:,0])
 
-    return
+    # get bonds, angles, dihedral
+    bonds, angles, dihedral = getPara(ii)
+    #print(bonds)
+    #print(angles)
+    #print(dihedral)
 
-filename = "smiles.txt"
-filenames = getSMILES(filename)
-#print(filenames)
-for smi_string in filenames:
-    readSMILES(smi_string)
-    break
-    
+    result["bonds"] = arrToDicts(bonds)
+    result["angles"] = arrToDicts(angles)
+    result["dihedral"] = arrToDicts(dihedral)
+
+    return result
+
+
+##### check the files
+#checkPara()
+
+###############
+"""
+ name:
+   atoms: str,list
+   bonds:
+        id: str,list
+        pair: array (n)
+        value: list,float
+   angles:
+        id: str,list
+        pair: int,list ( 3 or 5)
+        value: float,list
+   dihedral:
+        id: str,list
+        pair: int,list (n,4)
+        value: float,list
+"""
+size = len(filenames)
+#size = 100
+database = {}
+for i in range(size):
+    print("sample",i+1)
+    result = getSample(i)
+    name = filenames[i]
+    database[name] = result
+
+# dict to json
+database = json.dumps(database,indent=4)
+
+#### write the data
+filename = "inchikey_filter_database%s.json"%(sys.argv[1])
+filename = filename.replace("name2",'')
+filename = filename.replace(".txt",'')
+fp = open(filename,'w')
+fp.write(database)
+fp.close()
