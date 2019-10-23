cec591c726d4ea8038770f5c02b7512f298f8240
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:21 +0800

    add 132_tenGru_maskBefore

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -15,7 +15,7 @@ class LanNet(nn.Module):
         #self.layer0 = nn.Sequential()
         #self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layer1 = nn.Sequential()
-        self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True))
+        self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
 
         self.layer2 = nn.Sequential()
         self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
@@ -32,18 +32,24 @@ class LanNet(nn.Module):
         # get gru output
         out_hidden, hidd = self.layer1(src)
         # summation of the two hidden states in the same node
-        out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
+        # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
         #print(out_hidden.shape)
-        out_hidden = out_hidden.contiguous().view(-1, out_hidden.size(-1))   
+        # get  masked outputs
+        mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_hidden.size(2))
+        # output with new size (batch_size, hidden_dim)
+        out_hidden = out_hidden*mask
+        out_hidden = out_hidden.sum(dim=1)/mask.sum(dim=1)
+        #out_hidden = out_hidden.contiguous().view(-1, out_hidden.size(-1))   
         out_bn = self.layer2(out_hidden)
         out_target = self.layer3(out_bn)
 
 
-        out_target = out_target.contiguous().view(batch_size, fea_frames, -1)
-        mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_target.size(2))
-        out_target_mask = out_target * mask
-        out_target_mask = out_target_mask.sum(dim=1)/mask.sum(dim=1)
-        predict_target = F.softmax(out_target_mask, dim=1)
+        #out_target = out_target.contiguous().view(batch_size, fea_frames, -1)
+        #mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_target.size(2))
+        #out_target_mask = out_target * mask
+        #out_target_mask = out_target_mask.sum(dim=1)/mask.sum(dim=1)
+        predict_target = F.softmax(out_target, dim=1)
+        #print(predict_target.shape,target.shape)
 
         # 计算loss
         tar_select_new = torch.gather(predict_target, 1, target)
