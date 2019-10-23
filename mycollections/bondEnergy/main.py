bbad2cf0fb70282cfc2067cc31fb59352de51138
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:12 +0800

    add 042_atomSeq

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -1,61 +1,48 @@
 #!/usr/bin/python
 
-
-import openbabel as ob
 import numpy as np
 
-obConversion = ob.OBConversion()
-obConversion.SetInAndOutFormats("smi", "smi")
-mol = ob.OBMol()
+filename1 = "exist.txt"
+filename2 = "total.txt"
 
-def readSMILES(smi_string):
-    # read smiles
-    obConversion.ReadString(mol, smi_string)
-    # get bonds
-    MolBond = []
-    ChainBond = []
-    
-    #for angle in ob.OBMolAngleIter(mol):    
-    #    print(angle)
-    for angle in ob.OBMolTorsionIter(mol):    
-        print(angle)
-    
-    for bond in ob.OBMolBondIter(mol):    
-        a = bond.GetBeginAtomIdx()
-        b = bond.GetEndAtomIdx()
-        bo = bond.GetBondOrder()
-        MolBond.append((a,b,bo))
-    
-        if bond.IsInRing() or bond.IsAromatic():
-            continue
-    
-        ChainBond.append(bond.GetIdx())
-    
-    #print(MolBond)
-    #print("atom number",mol.NumAtoms())
-    #print("bond number",mol.NumBonds())
-    #print("residue number",mol.NumResidues())
-    #MolBond.sort()
-    #print(MolBond)
-    #NumOfRingBond = len(MolBond) - len(ChainBond)
-def getSMILES(filename):
-    fp = open(filename,'r')
-    data = fp.read()
-    fp.close()
-    data = data.split('\n')
-    data = data[:-1]
-    res = []
-    # get the first column
-    for line in data:
-        line = line.split(' ')
-        res.append(line[0])
+# get dicts
+dicts = {}
+data1 = np.loadtxt(filename1,dtype=str)
+data2 = np.loadtxt(filename2,dtype=str)
 
-    return res
+for i in data1:
+    dicts[i] = 0
 
-filename = "smiles.txt"
-filenames = getSMILES(filename)
-#print(filenames)
-for smi_string in filenames:
-    readSMILES(smi_string)
-    break
-    
+# statistics
+
+for i in data2:
+    dicts[i] += 1
+
+# count
+count = np.zeros(10)
+
+for key in dicts:
+    value = dicts[key]
+    #print(key,value)
+    count[value] += 1
+
+count = count.astype(int)
+# six repeats
+for key in dicts:
+    value = dicts[key]
+    if value == 6:
+        print(key)
+
+res = 0
+for i in range(10):
+    print("number ",i,count[i])
+    res += i*count[i]
+print(sum(count))
+print(res)
+
+import json
+dicts = json.dumps(dicts,indent=4)
+filename = "energyNumber.json"
+fp = open(filename,'w')
+fp.write(dicts)
+fp.close()
