60ef39f00631f6d96602879250df7a0de14d991b
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:14 +0800

    add 082_getParas

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -1,43 +1,71 @@
 #!/usr/bin/python
 
-
 import openbabel as ob
 import pybel 
 import numpy as np
 
 from getsmiles import getSMILES
+from getsmiles import readSMILES
+from getsmiles import readJson
 
-obConversion = ob.OBConversion()
-obConversion.SetInAndOutFormats("smi", "smi")
+import json
 
-def readSMILES(smi_string):
-    # read smiles
-    mol = ob.OBMol()
-    obConversion.ReadString(mol, smi_string)
-    new_mol = pybel.Molecule(mol)
-    return mol
-def getFingers(mols):
-    vec1 = ob.vectorUnsignedInt()
-    vec2 = ob.vectorUnsignedInt()
-    fingerprinter.GetFingerprint(mols[0], vec1)
+
+filename = "../inchikey_bonds.json"
+# residue: bond information
+residueBonds = readJson(filename)
+
+filename = "../new_filter3.json"
+# residue: "ID","molecule","type","energy"
+residueEnergies  = readJson(filename)
+
+filename = "../inchikey_filter_database.json"
+# ID : "atoms","bonds","angles","dihedrals"
+idParameters  = readJson(filename)
+
+# store the parameters
+paraDicts = {}
+
+"""
+../energyNumber.json
+../IDResidue.json
+../inchikey_bonds.json
+../inchikey_filter_database.json
+../molEnergyNumber.json
+../new_filter3.json
+../residuesMatchDicts.json
+"""
+
+# get the bonds connected to "C"
+for residue in residueBonds:
+    # index of the carbon and oxygen
+    indexC = residueBonds[residue][0]
+    indexO = residueBonds[residue][1]
+    print(indexC,indexO)
+    # get the id and molecule
+    id = residueEnergies[residue]['ID']
+    mol = residueEnergies[residue]['molecule']
+    print(mol)
+    # get the parameters
+    para = idParameters[id]
+    para = json.dumps(para,indent = 4)
+    print(para)
+    mol = readSMILES(mol)
+    # add hydrogens
+    mol.AddHydrogens()
     
-    for mol in mols:
-        fingerprinter.GetFingerprint(mol, vec2)
-        tanimoto_value = fingerprinter.Tanimoto(vec1,vec2)
-        print(tanimoto_value)
 
-def getAllMols(filename):
-    strings = getSMILES(filename)
-    mols = []
-    for name in strings:
-        mol = readSMILES(name)
-        mols.append(mol)
+    print(id)
 
-    return mols
+    # get the neighbor of the carbon
+    carbon = mol.GetAtom(indexC)
+    for carbonNeighbor in ob.OBAtomAtomIter(carbon):
+        print(carbonNeighbor.GetIdx())
 
+    break
 
-filename = "03_sixRepeats.smi"
-# get all smiles of the molecules
-mols = getAllMols(filename)
-getFingers(mols)
+# write the data
+# fp = open("inchikey_parameters.json",'w')
+# json.dump(paraDicts,fp,indent = 4)
+
 
