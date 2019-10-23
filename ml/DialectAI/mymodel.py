2c9882aa3563981b80eee19eadb94bddafaaac65
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:23 +0800

    add 272_two_kejiaMinnan_highway_correct

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from augmented_lstm import AugmentedLstm
+#from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence, PackedSequence
+from torch.nn.utils.rnn import pack_padded_sequence
 
 class LanNet(nn.Module):
     def __init__(self, input_dim=48, hidden_dim=2048, bn_dim=100, output_dim=10):
@@ -16,7 +18,7 @@ class LanNet(nn.Module):
         #self.layer0 = nn.Sequential()
         #self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layer1 = nn.Sequential()
-        self.layer1.add_module('gru', AugmentedLstm(self.input_dim, self.hidden_dim,recurrent_dropout_probability = 0.5,go_forward=False))
+        self.layer1.add_module('gru', AugmentedLstm(self.input_dim, self.hidden_dim,recurrent_dropout_probability = 0.5))
 
         self.layer2 = nn.Sequential()
         self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
@@ -30,10 +32,22 @@ class LanNet(nn.Module):
     def forward(self, src, mask, target):
         batch_size, fea_frames, fea_dim = src.size()
 
+        # frames
+        frames = torch.ones(batch_size)*fea_frames
+        frames = frames.type(torch.LongTensor)
+        frames = frames.cuda()
+        #print("frames",frames.size())
+        # pack the sequence input
+        src = pack_padded_sequence(src,frames,batch_first=True)
+        #print("src",src.data.size())
         # get augmented lstmoutput
         out_hidden, hidd = self.layer1(src)
+        out_hidden = out_hidden.data
+        # contiguous of out_hidden
+        out_hidden = out_hidden.contiguous().view(-1,batch_size,out_hidden.size(1))
+        # transpose
+        out_hidden = out_hidden.transpose(0,1)
 
-        #print(out_hidden.size())
         #print("out hidden",out_hidden.data.size())
         # summation of the two hidden states in the same node
         # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
