17a6573f6325cf6762d6fc3ae41beac5b71ccf28
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:22 +0800

    add 210_twoGru_KejiaAndMinnan_fb40_E

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -5,19 +5,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LanNet(nn.Module):
-    def __init__(self, input_dim=40, pitch_dim=13,hidden_dim=2048, bn_dim=100, output_dim=10,alpha=0.5):
+    def __init__(self, input_dim=48, hidden_dim=2048, bn_dim=100, output_dim=10):
         super(LanNet, self).__init__()
-        self.pitch_dim  = pitch_dim
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.bn_dim     = bn_dim
+        self.bn_dim = bn_dim
         self.output_dim = output_dim
-        self.alpha = alpha
 
-        # for pitch flux
-        self.layer0 = nn.Sequential()
-        self.layer0.add_module('gru', nn.GRU(self.pitch_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
-        # for fbank
+        #self.layer0 = nn.Sequential()
+        #self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
         self.layer1 = nn.Sequential()
         self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
 
@@ -29,17 +25,29 @@ class LanNet(nn.Module):
         self.layer3 = nn.Sequential()
         self.layer3.add_module('batchnorm', nn.BatchNorm1d(self.bn_dim))
         self.layer3.add_module('linear', nn.Linear(self.bn_dim, self.output_dim))
-    def getLoss(self,out_hidden,mask,target,fea_frames):
-        batch_size = out_hidden.size(0)
+
+    def forward(self, src, mask, target):
+        batch_size, fea_frames, fea_dim = src.size()
+
+        # get gru output
+        out_hidden, hidd = self.layer1(src)
+        # summation of the two hidden states in the same node
+        # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
+        #print(out_hidden.shape)
         # get  masked outputs
         mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_hidden.size(2))
         # output with new size (batch_size, hidden_dim)
         out_hidden = out_hidden*mask
         out_hidden = out_hidden.sum(dim=1)/mask.sum(dim=1)
         #out_hidden = out_hidden.contiguous().view(-1, out_hidden.size(-1))   
-        # shared weights
         out_bn = self.layer2(out_hidden)
         out_target = self.layer3(out_bn)
+
+
+        #out_target = out_target.contiguous().view(batch_size, fea_frames, -1)
+        #mask = mask.contiguous().view(batch_size, fea_frames, 1).expand(batch_size, fea_frames, out_target.size(2))
+        #out_target_mask = out_target * mask
+        #out_target_mask = out_target_mask.sum(dim=1)/mask.sum(dim=1)
         predict_target = F.softmax(out_target, dim=1)
         #print(predict_target.shape,target.shape)
 
@@ -47,24 +55,6 @@ class LanNet(nn.Module):
         tar_select_new = torch.gather(predict_target, 1, target)
         ce_loss = -torch.log(tar_select_new) 
         ce_loss = ce_loss.sum() / batch_size
-
-        return ce_loss,predict_target
-
-    def forward(self, src, mask, target):
-        batch_size, fea_frames, fea_dim = src.size()
-
-        # get gru output
-        out_hidden_pitch,hidd = self.layer0(src[:,:,self.input_dim:])
-        out_hidden_fb   ,hidd = self.layer1(src[:,:,:self.input_dim])
-        # summation of the two hidden states in the same node
-        # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
-        #print(out_hidden.shape)
-        # get the loss
-        loss_pitch,predict_target = self.getLoss(out_hidden_pitch,mask,target,fea_frames)
-        loss_fb   ,predict_target = self.getLoss(out_hidden_fb,mask,target,fea_frames)
-        
-        ce_loss = loss_pitch*self.alpha + loss_fb*(1 - self.alpha)
-
 
         # 计算acc
         (data, predict) = predict_target.max(dim=1)
--- ml/DialectAI/train.py
+++ ml/DialectAI/train.py
@@ -29,7 +29,7 @@ logging.basicConfig(level = logging.DEBUG,
 import torch
 import torch.utils.data as Data
 
-#from read_data import get_samples, get_data, TorchDataSet
+#from mydata import get_samples, get_data, TorchDataSet
 from mydata import  TorchDataSet
 from mymodel import LanNet
 
@@ -37,33 +37,30 @@ from mymodel import LanNet
 # data list
 # train
 train_list = "label_train_list_fb.txt"
+#train_list = "train_long_fb.txt"
 # dev
 dev_list   = "label_dev_list_fb.txt"
+#dev_list = "dev_long_fb.txt"
+# dev
 
 # basic configuration parameter
 use_cuda = torch.cuda.is_available()
 # network parameter 
-toneLengthD = 6
-pitch_dim  = 2*toneLengthD + 1# 40 before
-fb_dim     = 40 # 40 + 13
-dimension  = pitch_dim + fb_dim
+dimension = 41 # 40 before
 language_nums = 2 # 9!
-learning_rate = 0.1
+learning_rate = 0.01
 batch_size = 64
 chunk_num = 10
 #train_iteration = 10
-train_iteration = 12
+train_iteration = 10
 display_fre = 50
 half = 4
+# data augmentation
 
 # save the models
 import sys
-
-# mixing factor
-alpha = float(sys.argv[1])
 #model_dir = "models" + sys.argv[1]
-model_dir = "models"+str(alpha)
-print(model_dir)
+model_dir = "models"
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
@@ -78,7 +75,7 @@ dev_dataset = TorchDataSet(dev_list, batch_size, chunk_num, dimension)
 logging.info('finish reading all train data')
 
 # 优化器，SGD更新梯度
-train_module = LanNet(input_dim=fb_dim,pitch_dim=pitch_dim, hidden_dim=128, bn_dim=30, output_dim=language_nums,alpha=alpha)
+train_module = LanNet(input_dim=dimension, hidden_dim=128, bn_dim=30, output_dim=language_nums)
 logging.info(train_module)
 optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
 
