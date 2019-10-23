2764804786312692c94ec406ec523404ab266a11
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:20 +0800

    add 00_before/04.trainLSTM

--- ml/DialectAI/train.py
+++ ml/DialectAI/train.py
@@ -64,7 +64,7 @@ dev_dataset = TorchDataSet(dev_list, batch_size, chunk_num, dimension)
 logging.info('finish reading all train data')
 
 # 优化器，SGD更新梯度
-train_module = LanNet(input_dim=dimension, hidden_dim=128, bn_dim=30, output_dim=language_nums)
+train_module = LanNet(input_dim=dimension, hidden_dim=256, bn_dim=60, output_dim=language_nums)
 logging.info(train_module)
 optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
 
