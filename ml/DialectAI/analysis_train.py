e7667d6612385f90b2a5948b651938c1ac965ebb
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:22 +0800

    add 222_fourConv_hardFour_plp0_majority

--- ml/DialectAI/analysis_train.py
+++ ml/DialectAI/analysis_train.py
@@ -2,28 +2,22 @@
 
 import numpy as np
 
-from readhtk import HTKfile
+from read_data import HTKfile
 #train=False
-directory = "../labels/"
 def getFrames(train):
     if train:
-        name= directory + "label_train_list_fb.txt"
+        name="label_train_list_fb.txt"
     else:
-        name= directory + "label_dev_list_fb.txt"
+        name="label_dev_list_fb.txt"
     data = np.loadtxt(name,delimiter=' ',dtype=str)
     
     data = data[:,0]
     #print(data)
     frames = []
-    i = 0
     for htk_file in data:
         htk_feature = HTKfile(htk_file)
         feature_frames = htk_feature.get_frame_num()
-        i = i + 1
         frames.append(feature_frames)
-        if i%1000 == 0:
-            print("i = ",i)
-
     
     frames = np.array(frames)
     if train:
@@ -32,9 +26,9 @@ def getFrames(train):
         np.savetxt("dev_frames.txt",frames,fmt="%d")
 def getNames(train):
     if train:
-        name= directory + "label_train_list_fb.txt"
+        name="label_train_list_fb.txt"
     else:
-        name= directory + "label_dev_list_fb.txt"
+        name="label_dev_list_fb.txt"
     data = np.loadtxt(name,delimiter=' ',dtype=str)
     
     #data = data[:,0]
@@ -43,7 +37,7 @@ def getNames(train):
     short_frames = []
     for htk_file in data:
         htk_feature = HTKfile(htk_file[0])
-        #print(htk_feature.read_data().shape)
+        print(htk_feature.read_data().shape)
         feature_frames = htk_feature.get_frame_num()
         if feature_frames < 300:
             short_frames.append(htk_file)
@@ -59,6 +53,6 @@ def getNames(train):
         np.savetxt("dev_long_fb.txt",long_frames,fmt="%s")
         np.savetxt("dev_short_fb.txt",short_frames,fmt="%s")
 train=True
-getFrames(train)
+getNames(train)
 train=False
-getFrames(train)
+getNames(train)
--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -4,6 +4,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class baseConv1d(nn.Module):
+    def __init__(self,input_dim=40,output_dim=40):
+        super(baseConv1d, self).__init__()
+        self.input_dim=input_dim
+        self.output_dim=output_dim
+        self.conv1 = nn.Conv1d(self.input_dim,self.output_dim,kernel_size=3,padding=1)
+    def forward(self,x):
+
+        y = self.conv1(x)
+        y = F.relu(y)
+
+        return y
+
 class LanNet(nn.Module):
     def __init__(self, input_dim=48, hidden_dim=2048, bn_dim=100, output_dim=10):
         super(LanNet, self).__init__()
@@ -12,10 +25,8 @@ class LanNet(nn.Module):
         self.bn_dim = bn_dim
         self.output_dim = output_dim
 
-        #self.layer0 = nn.Sequential()
-        #self.layer0.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
-        self.layer1 = nn.Sequential()
-        self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
+        self.conv1 = baseConv1d(self.input_dim,self.hidden_dim)
+        self.conv2 = baseConv1d(self.hidden_dim,self.hidden_dim)
 
         self.layer2 = nn.Sequential()
         self.layer2.add_module('batchnorm', nn.BatchNorm1d(self.hidden_dim))
@@ -29,8 +40,15 @@ class LanNet(nn.Module):
     def forward(self, src, mask, target):
         batch_size, fea_frames, fea_dim = src.size()
 
+        # conv layer 
+        # transpose
+        src = src.transpose(1,2)
+        src = self.conv1(src)
+        src = self.conv2(src)
+
+        # transpose
+        out_hidden = src.transpose(1,2)
         # get gru output
-        out_hidden, hidd = self.layer1(src)
         # summation of the two hidden states in the same node
         # out_hidden = out_hidden[:,:,0:self.hidden_dim] + out_hidden[:,:,self.hidden_dim:]
         #print(out_hidden.shape)
@@ -39,6 +57,7 @@ class LanNet(nn.Module):
         # output with new size (batch_size, hidden_dim)
         out_hidden = out_hidden*mask
         out_hidden = out_hidden.sum(dim=1)/mask.sum(dim=1)
+        #out_hidden = out_hidden.sum(dim=1)/fea_frames
         #out_hidden = out_hidden.contiguous().view(-1, out_hidden.size(-1))   
         out_bn = self.layer2(out_hidden)
         out_target = self.layer3(out_bn)
@@ -58,10 +77,12 @@ class LanNet(nn.Module):
 
         # 计算acc
         (data, predict) = predict_target.max(dim=1)
+        prediction = predict
         predict = predict.contiguous().view(-1,1)
         correct = predict.eq(target).float()       
         num_samples = predict.size(0)
         sum_acc = correct.sum().item()
         acc = sum_acc/num_samples
 
+        #return acc, ce_loss,prediction
         return acc, ce_loss
