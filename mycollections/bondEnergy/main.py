48f149470df00ab82e0cf54b76da4a5a31dbe0ed
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:12 +0800

    add 028_getCorrentNumber

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -2,32 +2,38 @@
 
 
 import openbabel as ob
+import numpy as np
 
 obConversion = ob.OBConversion()
 obConversion.SetInAndOutFormats("smi", "smi")
 mol = ob.OBMol()
 
-# read smiles
-smi_string = "C1=CC=CC=C1"
+def readSMILES(smi_string):
+    # read smiles
+    obConversion.ReadString(mol, smi_string)
+    # get bonds
+    MolBond = []
+    ChainBond = []
+    
+    
+    for bond in ob.OBMolBondIter(mol):    
+        a = bond.GetBeginAtomIdx()
+        b = bond.GetEndAtomIdx()
+        bo = bond.GetBondOrder()
+        MolBond.append((a,b,bo))
+    
+        if bond.IsInRing() or bond.IsAromatic():
+            continue
+    
+        ChainBond.append(bond.GetIdx())
+    
+    print(MolBond)
+    #MolBond.sort()
+    #print(MolBond)
+    #NumOfRingBond = len(MolBond) - len(ChainBond)
 
-obConversion.ReadString(mol, smi_string)
-# get bonds
-MolBond = []
-ChainBond = []
-
-
-for bond in ob.OBMolBondIter(mol):    
-    a = bond.GetBeginAtomIdx()
-    b = bond.GetEndAtomIdx()
-    bo = bond.GetBondOrder()
-    MolBond.append((a,b,bo))
-
-    if bond.IsInRing() or bond.IsAromatic():
-        continue
-
-    ChainBond.append(bond.GetIdx())
-
-print(MolBond)
-MolBond.sort()
-print(MolBond)
-NumOfRingBond = len(MolBond) - len(ChainBond)
+filename = "smiles.txt"
+filenames = np.loadtxt(filename,delimiter=' ',dtype=str)
+for smi_string in filenames[:,0]:
+    readSMILES(smi_string)
+    
