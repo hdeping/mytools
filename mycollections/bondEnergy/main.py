6124d9e369680ce0526713d1cfce8fb9af95af0e
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 20:30:13 +0800

    add 050_atomSeq

--- mycollections/bondEnergy/main.py
+++ mycollections/bondEnergy/main.py
@@ -1,62 +1,46 @@
 #!/usr/bin/python
 
-
-import openbabel as ob
 import numpy as np
+import json
 
-obConversion = ob.OBConversion()
-obConversion.SetInAndOutFormats("smi", "smi")
-mol = ob.OBMol()
 
-def readSMILES(smi_string):
-    # read smiles
-    obConversion.ReadString(mol, smi_string)
-    # get bonds
-    MolBond = []
-    ChainBond = []
-    
-    #for angle in ob.OBMolAngleIter(mol):    
-    #    print(angle)
-    for angle in ob.OBMolTorsionIter(mol):    
-        print(angle)
-    
-    for bond in ob.OBMolBondIter(mol):    
-        a = bond.GetBeginAtomIdx()
-        b = bond.GetEndAtomIdx()
-        bo = bond.GetBondOrder()
-        MolBond.append((a,b,bo))
-    
-        if bond.IsInRing() or bond.IsAromatic():
-            continue
-    
-        ChainBond.append(bond.GetIdx())
-    
-    #print(MolBond)
-    #print("atom number",mol.NumAtoms())
-    #print("bond number",mol.NumBonds())
-    #print("residue number",mol.NumResidues())
-    #MolBond.sort()
-    #print(MolBond)
-    #NumOfRingBond = len(MolBond) - len(ChainBond)
-def getSMILES(filename):
+def readJson(filename):
     fp = open(filename,'r')
-    data = fp.read()
+    molecules = json.load(fp)
     fp.close()
-    data = data.split('\n')
-    data = data[:-1]
-    res = []
-    # get the first column
-    for line in data:
-        line = line.split(' ')
-        res.append(line[0])
 
-    return res
+    return molecules
 
-filename = "smiles.txt"
-filenames = getSMILES(filename)
+def writeJson(filename,database):
+    database = json.dumps(database,indent=4)
+    fp = open(filename,'w')
+    fp.write(database)
+    fp.close()
 
-#print(filenames)
-for smi_string in filenames:
-    readSMILES(smi_string)
-    break
-    
+filename = "../new_filter3.json"
+molecules = readJson(filename)
+
+id_dicts = {}
+# initial 
+#   id :[residues]
+#   mol:[residues]
+       
+for residue in molecules:
+    id  = molecules[residue]['ID']
+    mol = molecules[residue]['molecule']
+    id_dicts[id]  = []
+    id_dicts[mol] = []
+
+for residue in molecules:
+    id  = molecules[residue]['ID']
+    id_dicts[id].append(residue)
+
+
+for residue in molecules:
+    id  = molecules[residue]['ID']
+    mol = molecules[residue]['molecule']
+    id_dicts[mol] = id_dicts[id]
+
+
+filename = "IDResidue.json"
+writeJson(filename,id_dicts)
