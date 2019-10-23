7e5695745e4fc75ec635d9c40e4ea9043d254c6a
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:21 +0800

    add 00_before/30_sgd_dataAugm_allData

--- ml/DialectAI/train.py
+++ ml/DialectAI/train.py
@@ -72,9 +72,10 @@ logging.info('finish reading all train data')
 # 优化器，SGD更新梯度
 train_module = LanNet(input_dim=dimension, hidden_dim=128, bn_dim=30, output_dim=language_nums)
 logging.info(train_module)
-#optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
-optimizer = torch.optim.Adam(train_module.parameters(), lr=learning_rate, betas=(0.9,0.999),eps=1e-8)
+optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
 
+# initialize the model
+train_module.load_state_dict(torch.load("models_data10/model2.model"))
 #device = torch.device("cuda:2")
 # 将模型放入GPU中
 if use_cuda:
@@ -89,8 +90,8 @@ for epoch in range(train_iteration):
     print("epoch",epoch)
     if epoch >= half:
         learning_rate /= 2.
-        #optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
-        optimizer = torch.optim.Adam(train_module.parameters(), lr=learning_rate, betas=(0.9,0.999),eps=1e-8)
+        optimizer = torch.optim.SGD(train_module.parameters(), lr=learning_rate, momentum=0.9)
+        #optimizer = torch.optim.Adam(train_module.parameters(), lr=learning_rate, betas=(0.9,0.999),eps=1e-8)
 
 ##  train
     train_dataset.reset()
