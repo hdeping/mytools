652785561299e79278173382709bd1a1cf359f3f
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-10-23 21:33:21 +0800

    add 00_before/78_stride_1-5-10-20-40

--- ml/DialectAI/mymodel.py
+++ ml/DialectAI/mymodel.py
@@ -5,19 +5,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class baseFC(nn.Module):
-    def __init__(self,input_chanel,output_chanel):
-        super(baseFC,self).__init__()
+class baseConv1d(nn.Module):
+    def __init__(self,input_chanel,output_chanel,kernel_size,stride,padding):
+        super(baseConv1d,self).__init__()
         # architeture of the base conv1d
-        self.fc = nn.Linear(input_chanel,output_chanel)
+        self.conv = nn.Conv1d(input_chanel,output_chanel,kernel_size=kernel_size,stride=stride,padding=padding)
         self.bn   = nn.BatchNorm1d(output_chanel)
     def forward(self,x):
         # conv 
-        x = self.fc(x)
+        x = self.conv(x)
         # batchnorm 
         x = self.bn(x)
         # 1d avg pool 
-        # x = F.avg_pool1d(x,kernel_size=2)
+        x = F.avg_pool1d(x,kernel_size=2)
         # relu output
         x = F.relu(x)
         return x
@@ -30,12 +30,12 @@ class LanNet(nn.Module):
         self.bn_dim = bn_dim
         self.output_dim = output_dim
 
-        self.layer_fc = nn.Sequential()
-        self.layer_num = 2
+        self.layer_conv = nn.Sequential()
+        chanels = [1,5,10,20,40]
         #chanels = [1,2,4,8,16,32,40,64,80]
-        chanels = [400,100,40]
+        self.layer_num = 4
         for i in range(self.layer_num):
-            self.layer_fc.add_module('fc'+str(i),baseFC(chanels[i],chanels[i+1]))
+            self.layer_conv.add_module('conv'+str(i),baseConv1d(chanels[i],chanels[i+1],3,2,1))
 
         self.layer1 = nn.Sequential()
         self.layer1.add_module('gru', nn.GRU(self.input_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=False))
@@ -52,11 +52,12 @@ class LanNet(nn.Module):
     def forward(self, x, mask, target):
         batch_size, fea_frames, fea_dim = x.size()
         # reshape the input
-        x = x.contiguous().view(batch_size*fea_frames,-1)
-        # fc layer
-        x = self.layer_fc(x)
-        # reshape the x
+        x = x.contiguous().view(batch_size*fea_frames,1,-1)
+        # conv layer
+        x = self.layer_conv(x)
+        # reshape x
         x = x.contiguous().view(batch_size,fea_frames,-1)
+
         # RNN layer
         out_hidden, hidd = self.layer1(x)
         #print(out_hidden.data.shape)
--- ml/DialectAI/try_model.py
+++ ml/DialectAI/try_model.py
@@ -2,9 +2,10 @@ from mymodel import baseConv1d
 import torch
 
 conv = []
-chanels = [1,2,4,8,16,32,40,64,80]
+#chanels = [1,2,4,8,16,32,40,64,80]
+chanels = [1,5,10,20,40]
 # get conv layers
-layer_num = 8
+layer_num = 4
 for i in range(layer_num):
     conv_layer = baseConv1d(chanels[i],chanels[i+1],3,2,1)
     conv.append(conv_layer)
