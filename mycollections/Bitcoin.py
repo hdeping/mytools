f434a5ddc531a65617d78467a50467a61f3d478f
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-12-11 16:12:30 +0800

    add self.getDate

--- mycollections/Bitcoin.py
+++ mycollections/Bitcoin.py
@@ -16,9 +16,10 @@
 
 import json
 import os
+from mytools import MyCommon
+import time
 
-
-class Bitcoin():
+class Bitcoin(MyCommon):
     """
     wrapper for bitcoin-cli"""
     def __init__(self):
@@ -28,9 +29,41 @@ class Bitcoin():
         """
         super(Bitcoin, self).__init__()
         self.command = "bitcoin-cli getblock `bitcoin-cli getblockhash %d`"
+        self.keys    = ["size","weight","height",
+                        "time","nonce","ntx","difficulty"]
         return
+
+    def getInfo(self,content):
+        dicts = {}
+        for key in self.keys:
+            dicts[key] = content[key]
+
+        return dicts
+
+    def getDate(self,timestamp):
+        """
+        timestamp: 
+            such as 1354216278
+        return:
+            date 
+        """
+        timeArray = time.localtime(timestamp)
+        date = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
+
+        return date 
     def run(self):
-        for i in range(600000)
+        for i in range(0,1,100000):
+            command = self.command % (i)
+            content = os.popen(command).read()
+            content = json.loads(content)
+
+            print(i,content)
+
+
         return
+
+
+bitcoin = Bitcoin()
+bitcoin.run()
 
         
