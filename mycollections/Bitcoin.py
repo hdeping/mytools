6f6c96e5e3d34276901a3b50adffcb6d25661d7f
黄德平(Deping Huang, lab PC) <xiaohengdao@gmail.com>
2019-12-12 15:52:20 +0800

    add method self.test

--- mycollections/Bitcoin.py
+++ mycollections/Bitcoin.py
@@ -18,6 +18,9 @@ import json
 import os
 from mytools import MyCommon
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Bitcoin(MyCommon):
     """
@@ -74,9 +77,62 @@ class Bitcoin(MyCommon):
         print(time.time() - t1)
 
         return
+    def test(self):
+        filename = "bitcoin.json"
+        data = self.loadJson(filename)
+        # print(data)
+        y = []
+        z = []
+        blockTime = []
+        for key in data:
+            y.append(data[key]["nTx"])
+            z.append(data[key]["difficulty"])
+            blockTime.append(data[key]["time"])
+        # print(y)
+        freq = 1000
+        num = (len(y) // freq)*freq
+        y = np.array(y[:num])
+        y = y.reshape((-1,freq))
+        y = np.average(y,axis=1)
+        x = np.arange(len(y))*10*freq/60/24/365
+        
+        z = np.array(z[:num])
+        z = z.reshape((-1,freq))
+        z = np.average(z,axis=1)
+        # z = np.log10(z)
+        # y = np.log10(y)
+
+        plt.semilogy(x,y,lw = self.width)
+        plt.semilogy(x,z,lw = self.width)
+        plt.show()
+        blockTime = np.array(blockTime)
+        blockTime = blockTime / 60
+        print(blockTime)
+        blockTime = blockTime[1:] - blockTime[:-1]
+        
+        #plt.hist(blockTime)
+        # plt.show()
+        print(blockTime)
+        print(min(blockTime))
+        print(max(blockTime))
+        for i,item in enumerate(blockTime):
+            if item < 0:
+                print(i,item)
+                break
+        print("average time",np.average(blockTime))
+        sections = np.arange(20)
+        cuts = pd.cut(blockTime,sections)
+        counts = pd.value_counts(cuts)
+        print(counts)
+        counts.plot.bar()
+        # plt.show()
+
+
+        return
 
 
 bitcoin = Bitcoin()
-bitcoin.run()
+# bitcoin.run()
+bitcoin.test()
 
         
