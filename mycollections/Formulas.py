9e19608abfde87fc3dd36e59d85191324b8da76c
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-24 18:00:24 +0800

    add self.narciTest2

--- mycollections/Formulas.py
+++ mycollections/Formulas.py
@@ -6619,6 +6619,19 @@ class Formulas(MyCommon,EllipticCurve):
                 print(num)
         return
 
+    def dealNarci(self,arr):
+        """
+        docstring for dealNarci
+        """
+        num = 0 
+        for i in arr:
+            num += self.calTable[i]
+
+        digits = self.num2Digits(num)
+        digits.sort()
+        if list(arr) == list(digits):
+            print(num)
+        return
     def narcissisticNumber(self,m = 2):
         """
         docstring for narcissisticNumber
@@ -6633,20 +6646,17 @@ class Formulas(MyCommon,EllipticCurve):
         print("there are %d items"%(num))
         count = 0
 
-        totalArr = []
+        self.calTable = [0,1]
+        for i in range(2,10):
+            self.calTable.append((Integer(i))**m)
+
         for line in tqdm(combinations):
             count += 1
             arr = []
             for i in range(m):
                 j = line[-i-1] + i - m + 1 
                 arr.append(j)
-            totalArr.append(arr)
-
-        totalArr = np.array(totalArr)
-
-        for i in range(4):
-            self.dealNarcissistic(totalArr,i)
-
+            # self.dealNarci(arr)
             
 
 
@@ -6659,38 +6669,112 @@ class Formulas(MyCommon,EllipticCurve):
         """
         docstring for testNarcissistic
         """
-       
 
         costTime = 0 
-        for i in range(21,39):
+        for i in range(30,31):
             m = i
             num = self.getCombinator(10+m-1,m)
             cost = num/150000/60
             costTime += cost
             print(i,cost,costTime)
-            # self.narcissisticNumber(m=i)
+            self.narcissisticNumber(m=i)
             # print("i = ",i)
             # for j in res:
             #     print(j)
         return
 
-    def narciTest1(self):
+    def narciTest1(self,a):
         """
         test for narcissisticNumber
         """
-        a = 63105425988599693916 
-        # a = 28361281321319229463398
+        # a = 63105425988599693916 
         digits = self.num2Digits(a)
         m = len(digits)
-        calTable = [0,1]
+        self.calTable = [0,1]
         for i in range(2,10):
-            calTable.append((Integer(i))**m)
+            self.calTable.append((Integer(i))**m)
         total = 0 
         for i in digits:
-            total += calTable[i] 
-        print(total)
+            total += self.calTable[i] 
 
+        return m,total
 
+    def narciTest2(self):
+        """
+        docstring for narciTest2
+        """
+        narciNumbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 
+                 370, 371, 407, 1634, 8208, 9474, 
+                 54748, 92727, 93084, 548834,1741725, 
+                 4210818, 9800817, 9926315, 24678050, 
+                 24678051, 88593477,146511208, 472335975, 
+                 534494836, 912985153,4679307774,
+                 32164049650, 32164049651, 40028394225, 
+                 42678290603, 44708635679, 49388550606, 
+                 82693916578, 94204591914, 28116440335967, 
+                 4338281769391370, 4338281769391371,
+                 21897142587612075, 35641594208964132, 
+                 35875699062250035,1517841543307505039, 
+                 3289582984443187032, 4498128791164624869, 
+                 4929273885928088826,63105425988599693916,
+                 128468643043731391252, 449177399146038697307,
+                 21887696841122916288858, 27879694893054074471405, 
+                 27907865009977052567814, 28361281321319229463398, 
+                 35452590104031691935943,
+                 174088005938065293023722, 
+                 188451485447897896036875, 
+                 239313664430041569350093,
+                 1550475334214501539088894, 
+                 1553242162893771850669378, 
+                 3706907995955475988644380, 
+                 3706907995955475988644381, 
+                 4422095118095899619457938,
+                 121204998563613372405438066, 
+                 121270696006801314328439376, 
+                 128851796696487777842012787, 
+                 174650464499531377631639254,
+                 177265453171792792366489765,
+                 14607640612971980372614873089,
+                 19008174136254279995012734740, 
+                 19008174136254279995012734741,
+                 23866716435523975980390369295,
+                 1145037275765491025924292050346, 
+                 1927890457142960697580636236639, 
+                 2309092682616190307509695338915,
+                 17333509997782249308725103962772,
+                 186709961001538790100634132976990,
+                 186709961001538790100634132976991,
+                 1122763285329372541592822900204593,
+                 12639369517103790328947807201478392,
+                 12679937780272278566303885594196922,
+                 1219167219625434121569735803609966019,
+                 12815792078366059955099770545296129367,
+                 115132219018763992565095597973971522400, 
+                 115132219018763992565095597973971522401]
+
+        stati = {}
+        for index,i in enumerate(narciNumbers):
+            m,res = self.narciTest1(i)
+            print("%d & %d & %d "%(index+1,m,i))
+            if m in stati:
+                stati[m] += 1 
+            else:
+                stati[m] = 1
+
+        count = 0
+        for key in stati:
+            count += 1
+            print("%d & %d & %d "%(count, key, stati[key]))
+        return
+
+    def ramanujanHardyFormula(self):
+        """
+        docstring for ramanujanHardyFormula
+        """
+        for n in range(10,51):
+            s = 1/(4*n*3**0.5)
+            s = s*np.exp(np.pi*(2*n/3)**0.5)
+            print(n,s)
         return
     def test(self):
         """
@@ -6703,7 +6787,9 @@ class Formulas(MyCommon,EllipticCurve):
         # self.friendlyNumberPair()
         # self.funnyNumbers()
         # self.testNarcissistic()
-        self.narciTest1()
+        # self.narciTest1()
+        # self.ramanujanHardyFormula()
+        self.narciTest2()
 
         return
 
