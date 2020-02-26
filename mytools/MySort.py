4eedb9696bad6587aca0f2982119a3d210f3f3f5
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-26 14:05:38 +0800

    add MySort.py and class MySort

---
+++ mytools/MySort.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-02-26 14:03:59
+    @project      : my practice for sorting algorithms
+    @version      : 0.1
+    @source file  : MySort.py
+
+============================
+"""
+
+class MySort():
+    """
+    my practice for sorting algorithms """
+    def __init__(self):
+        super(MySort, self).__init__()
+
+        return 
+    
+
+mySort = MySort()
+mySort.test()
+        
