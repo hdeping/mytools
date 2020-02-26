f1e644257c6ad5ccb74dbdcc331e0c273861aa99
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-26 14:53:34 +0800

    modify self.mergeSort with changing the position of if count == num: break

--- mytools/MySort.py
+++ mytools/MySort.py
@@ -43,10 +43,8 @@ class MySort():
             arr1 = self.mergeSort(arr[:num])
             arr2 = self.mergeSort(arr[num:])
             count = 0
-            print(arr1,arr2)
+            # print(arr1,arr2)
             for index,i in enumerate(arr2):
-                if count == num:
-                    break
                 tmp = arr1[count:].copy()
                 for j in tmp:
                     if i < j:
@@ -55,9 +53,9 @@ class MySort():
                     else:
                         res.append(j)
                         count += 1
+                if count == num:
+                    break
             if count == num:
-                if index < len(arr2) - 1:
-                    index = index - 1
                 res += arr2[index:]
             else:
                 res += arr1[count:]
@@ -67,9 +65,10 @@ class MySort():
         """
         docstring for test
         """
-        arr = [3,5,9,1,10,200,2000,-9,20,50,60,55,11]
-        # arr = [3,5,9,1,10,200,2000,-9]
-        # arr = [3,5,9,1,3,1,1,1]
+        arr = [3,5,9,1,10,200,2000,-9,10,200,
+               2000,-9,20,50,60,55,11]
+        # arr = [3,5,9,1,10,200,2000,-9,20,50]
+        # arr = [3,5,9,1]
         # arr = [3,9,7]
         print(arr)
         arr = self.mergeSort(arr)
