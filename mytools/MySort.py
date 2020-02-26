de114979bd8aff79a24ccc8e5edf29b7cb328650
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-26 14:42:42 +0800

    fixed with index < len(arr2) - 1

--- mytools/MySort.py
+++ mytools/MySort.py
@@ -43,11 +43,11 @@ class MySort():
             arr1 = self.mergeSort(arr[:num])
             arr2 = self.mergeSort(arr[num:])
             count = 0
-            # print(arr1,arr2)
+            print(arr1,arr2)
             for index,i in enumerate(arr2):
-                tmp = arr1[count:].copy()
                 if count == num:
                     break
+                tmp = arr1[count:].copy()
                 for j in tmp:
                     if i < j:
                         res.append(i)
@@ -56,7 +56,9 @@ class MySort():
                         res.append(j)
                         count += 1
             if count == num:
-                res += arr2[index-1:]
+                if index < len(arr2) - 1:
+                    index = index - 1
+                res += arr2[index:]
             else:
                 res += arr1[count:]
 
@@ -66,7 +68,8 @@ class MySort():
         docstring for test
         """
         arr = [3,5,9,1,10,200,2000,-9,20,50,60,55,11]
-        arr = [3,5,9,1,10,200,2000,-9,20,50,60]
+        # arr = [3,5,9,1,10,200,2000,-9]
+        # arr = [3,5,9,1,3,1,1,1]
         # arr = [3,9,7]
         print(arr)
         arr = self.mergeSort(arr)
