a70bcf009758726af73ad0c3613c06cee3cd3383
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-26 20:32:10 +0800

    add Algorithms.py and self.test

---
+++ mycollections/Algorithms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-02-26 20:30:26
+    @project      : practice for algorithms
+    @version      : 0.1
+    @source file  : Algorithms.py
+
+============================
+"""
+
+import numpy as np
+
+class Algorithms():
+    """docstring for Algorithms"""
+    def __init__(self):
+        super(Algorithms, self).__init__()
+    def test(self):
+        """
+        docstring for test
+        """
+
+        return
+        
+algo = Algorithms()
+algo.test()
