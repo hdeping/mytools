e3a4d2e071effa5832f017c31f64002be2359850
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-29 17:33:04 +0800

    add class Puzzles, and self.abSeven, self.test

--- mycollections/Puzzles.py
+++ mycollections/Puzzles.py
@@ -17,3 +17,43 @@
 
 from sympy import *
 import itertools
+import numpy as np
+
+
+class Puzzles():
+    """
+    solutions for math puzzles
+    """
+    def __init__(self):
+        super(Puzzles, self).__init__()
+    def abSeven(self):
+        """
+        docstring for abSeven
+        """
+        arr = np.arange(100)
+        lines = itertools.combinations(arr,2)
+
+        num = 7**7
+        for (a,b) in lines:
+            p1 = (a%7 == 0)
+            p2 = (b%7 == 0)
+            p3 = ((a+b)%7 == 0)
+            if p1 or p2 or p3:
+                continue
+
+            target = (a+b)**7 - a**7 - b**7
+            if target%num == 0:
+                print(a,b,target,num)
+
+        return
+
+    def test(self):
+        """
+        docstring for test
+        """
+        self.abSeven()
+        return
+
+puzzle = Puzzles()
+puzzle.test()
+        
