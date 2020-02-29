5e8aec1759c708a0f3b658341e08574bed55c962
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-02-29 17:26:15 +0800

    add Puzzles.py

---
+++ mycollections/Puzzles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-02-29 17:25:40
+    @project      : solutions for math puzzles
+    @version      : 1.0
+    @source file  : Puzzles.py
+
+============================
+"""
+
+from sympy import *
+import itertools
