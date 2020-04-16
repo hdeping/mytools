d1cfc08dc9725c2ae0738936ce6435678be91b5d
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-04-16 23:25:34 +0800

    add CellularAutomata.py add class CellularAutomata add self.test in CellularAutomata

---
+++ mycollections/CellularAutomata.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-04-16 23:24:13
+    @project      : code for cellular automata
+    @version      : 1.0
+    @source file  : CellularAutomata.py
+
+============================
+"""
+
+class CellularAutomata():
+    """docstring for CellularAutomata"""
+    def __init__(self):
+        super(CellularAutomata, self).__init__()
+    def test(self):
+        """
+        docstring for test
+        """
+        
+        return
+
+celluar = CellularAutomata()
+celluar.test()
+        
