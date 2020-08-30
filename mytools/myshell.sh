82b9249d9663d93b084a31b7ba0e2c612699d5ef
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-08-30 21:40:14 +0800

    add Makefile_sample

--- mytools/myshell.sh
+++ mytools/myshell.sh
@@ -4,3 +4,20 @@ do
     echo "mounting node$i"
     ssh node$i "mount 192.168.10.254:/home /home"
 done
+
+# ------ Makefile_sample --------
+gsl = gcc -g -o exe
+run:
+    $(gsl) main.c 
+    ./exe
+2:
+    gcc -O2 main.c -o exe
+    ./exe
+g:
+    gdb exe
+p:
+    gnuplot pic.sh
+    gspng
+    crop_png pic.png
+    rm pic.png && mv newpic.png pic.png
+    eog pic.png
