b9a5d5eeb5214d766af43907ae9e09b117eeb2bd
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-02 10:55:10 +0800

    add class GetLines in utils.py

--- mytools/utils.py
+++ mytools/utils.py
@@ -15,6 +15,7 @@
 ============================
 """
 import os
+import numpy as np
 
 class GetDoc():
     """
@@ -85,6 +86,59 @@ class GetDoc():
             
         return
 
+class GetLines():
+    """docstring for GetLines"""
+    def __init__(self):
+        super(GetLines, self).__init__()
+
+    def splitNames(self):
+        """
+        docstring for splitNames
+        split filenames into 2d array
+        """
+        
+        results = []
+        index = 0
+        num = 1000
+        while index < len(self.filenames):
+            results.append(self.filenames[index:index+num])
+            index += num
+        self.filenames = results
+        return
+    def run(self):
+        """
+        docstring for run
+        get the total lines of the code
+        """
+        self.filenames = np.loadtxt("mv",str)
+        self.splitNames()
+        print(len(self.filenames))
+        # print(self.filenames)
+
+        results = []
+        for line in self.filenames:
+            command = "wc -l %s"%(" ".join(line))
+            value   = self.getValue(command)
+            results.append(value)
+
+        print(sum(results))
+        
+            
+        return
+    def getValue(self,command):
+        """
+        docstring for getValue
+        input: command, a linux command based on wc
+        return: total lines of the code
+        """
+        
+        output = os.popen(command).read()
+        output = output.split("\n")
+        output = output[-2]
+        output = output.split(" ")
+        output = int(output[-2])
+        print(output)
+        return output
 
 
 
