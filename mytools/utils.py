37ae14d7708ceaacfee324cf6bf64d1f4967c4ef
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-02 10:54:17 +0800

    add class GetDoc in utils.py

---
+++ mytools/utils.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-09-02 10:52:59
+    @project      : my toolbox
+    @version      : 1.0
+    @source file  : utils.py
+
+============================
+"""
+import os
+
+class GetDoc():
+    """
+    docstring for GetDoc
+    convert the docstrings into  a README file
+    """
+    def __init__(self):
+        """
+        self.modules:
+            It is a string array
+            module name and their members
+            such as ["mytools","DrawCurve"...]
+            The first item should the module name,
+            and the others are the members.
+        self.outputName:
+            It is the name for output file,
+            initialized by "api.md"
+        """
+        super(GetDoc, self).__init__()
+        
+        self.modules = ["mytools",
+                        "DrawCurve",
+                        "DrawPig",
+                        "Excel",
+                        "MyCommon",
+                        "NameAll",
+                        "Triangle",
+                        "TurtlePlay"]
+        self.ouputName = "api.md"
+
+    def setOutputName(self,outputName): 
+        """
+        reset self.outputName
+        """
+        print("output name is set to ",outputName)
+        
+        self.ouputName = outputName
+        return
+
+    def getNewModule(self):
+        """
+        It is generater for the module name
+        for example, mytools --> mytools
+                    Triangle --> mytools.Triangle
+        """
+        for i in range(len(self.modules)):
+            name = self.modules[i]
+            if i == 0: 
+                yield name
+            else:
+                name = "%s.%s"%(self.modules[0],name)
+                yield name
+
+        return
+    def output(self):
+        """
+        convert the docstrings into a txt file with 
+        the command pydoc
+        """
+        for name in self.getNewModule():
+            if name == self.modules[0]:
+                command = "pydoc %s > %s"%(name,self.ouputName)
+            else:
+                command = "pydoc %s >> %s"%(name,self.ouputName)
+            print(command)
+            os.system(command)
+            
+            
+        return
+
+
+
+
+
