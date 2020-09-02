c30592d1a0640bd7f656f0af074672801503509e
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-02 09:59:00 +0800

    add class AnalyzeTree in utils.py

--- mycollections/AnalyzeTree.py
+++ mycollections/AnalyzeTree.py
@@ -14,36 +14,6 @@
 
 ============================
 """
-from xml.etree.ElementTree import parse
-from xml.etree.ElementTree import ElementTree
-
-class AnalyzeTree(object):
-    """docstring for AnalyzeTree"""
-    def __init__(self):
-        super(AnalyzeTree, self).__init__()
-    
-        self.root = parse('descript.xml')
-
-    def showTree(self,rootNode):
-        items = rootNode.findall("./")
-        if len(items) == 0:
-            print(rootNode.tag,rootNode.text)
-        else:
-            print("---------")
-            if type(rootNode) != ElementTree:
-                print(rootNode.tag)
-
-            for item in items:
-                self.showTree(item)
-
-        return 
-
-    def test(self):
-        self.dicts = []
-        self.showTree(self.root)
-        packages = self.root.findall("./")[-1]
-
-        return 
 
 tree = AnalyzeTree()
 tree.test()
--- mycollections/utils.py
+++ mycollections/utils.py
@@ -19,6 +19,36 @@ import numpy as np
 import json
 from mytools import MyCommon
 import re
+from xml.etree.ElementTree import parse
+from xml.etree.ElementTree import ElementTree
+
+class AnalyzeTree(object):
+    """docstring for AnalyzeTree"""
+    def __init__(self):
+        super(AnalyzeTree, self).__init__()
+    
+        self.root = parse('descript.xml')
+
+    def showTree(self,rootNode):
+        items = rootNode.findall("./")
+        if len(items) == 0:
+            print(rootNode.tag,rootNode.text)
+        else:
+            print("---------")
+            if type(rootNode) != ElementTree:
+                print(rootNode.tag)
+
+            for item in items:
+                self.showTree(item)
+
+        return 
+
+    def test(self):
+        self.dicts = []
+        self.showTree(self.root)
+        packages = self.root.findall("./")[-1]
+
+        return 
 
 class Ris2Bib():
     """
@@ -175,7 +205,6 @@ class Ris2Bib():
         self.printLines(lines)
         
         return
-
 
 class TianGanDiZhi(MyCommon):
     """
