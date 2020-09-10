6ebd9ff59172af7626c8611a66f975312553d746
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-10 10:40:23 +0800

    add fun changeBgImage import AlertDialog and Log import android.webkit.* import android.os.Bundle add wake-lock in the xml import android.widget.Toast add ids from main_activity

--- android/tools.kt
+++ android/tools.kt
@@ -13,3 +13,27 @@
 
 */
 
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AlertDialog.Builder
+import android.util.Log
+import android.webkit.*
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+fun changeBgImage(yuanyuan:ImageView){
+    var images:ArrayList<Int> = ArrayList()
+    images.add(R.drawable.yuanyuan6465)
+    images.add(R.drawable.yuanyuan6475)
+    images.add(R.drawable.yuanyuan6596)
+    images.add(R.drawable.yuanyuan6597)
+    images.add(R.drawable.yuanyuan6601)
+    images.add(R.drawable.yuanyuan6602)
+    var count = 0
+    yuanyuan.setOnClickListener{
+        count += 1
+        count = count% images.size
+        yuanyuan.setImageResource(images[count])
+    }
+}
