3eeba506d50284dc91bfadb0ecdcf0cbb577e6a9
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-10 10:25:46 +0800

    add tools.kt add AndroidManifest.xml

---
+++ android/tools.kt
@@ -0,0 +1,15 @@
+/*
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-09-10 10:22:41
+    @project      : tools for android develpment
+    @version      : 1.0
+    @source file  : tools.kt
+
+============================
+
+*/
+
