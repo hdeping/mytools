d003bc777c51f546863398d7340ec97337bac58c
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-25 16:58:55 +0800

    add errors.kt

---
+++ android/errors.kt
@@ -0,0 +1,12 @@
+2020年09月25日 16:55:41
+Failed to find GeneratedAppGlideModule.
+You should include an annotationProcessor compile 
+dependency on com.github.bumptech.glide:compiler 
+in your application and a @GlideModule annotated 
+AppGlideModule implementation or LibraryGlideModules 
+will be silently ignored
+2020-09-25 16:57:39.824 11778-11778/
+com.example.imagereader I/ViewTarget: Glide treats 
+LayoutParams.WRAP_CONTENT as a request for an 
+image the size of this device's screen dimensio
+glide错误
